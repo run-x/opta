aa34473b850da8e4b7c833eebdf921fb3330c972
Ankur Dahiya <ankur@runx.dev>
2020-12-15 23:20:57 -0800

    use the new abs

--- runxc/cli.py
+++ runxc/cli.py
@@ -1,12 +1,87 @@
 import json
 import os
 import shutil
+import subprocess
 from typing import Any, Mapping
 
 import click
 from PyInquirer import prompt
 
 TEMPLATE_DIR = os.environ.get("RUNXC_TEMPLATE_DIR")
+
+
+class Module:
+    def __init__(self, path: str):
+        self.path = path
+        self.config = json.loads(open(f"{path}/module.json").read())
+        # TODO should do better than just gitsha
+        self.version = (
+            subprocess.run(
+                ["git", "rev-parse", "HEAD"], capture_output=True, cwd=path, check=True
+            )
+            .stdout.decode("utf-8")
+            .strip()
+        )
+
+    def execute(self, outputDir: str, root: bool = True) -> Mapping[str, Any]:
+        """
+            Print name
+            Ask arguments
+            Copy files
+            Append to readme
+            Ask about submodules and recurse
+            Add info to root config
+        """
+        print(self.config["description"])
+
+        arguments = {}
+        if "arguments" in self.config:
+            arguments = prompt(self.config["arguments"])
+
+        if "files" in self.config:
+            for file in self.config["files"]:
+                if os.path.isdir(f"{self.path}/{file}"):
+                    shutil.copytree(
+                        f"{self.path}/{file}", f"{outputDir}/{file}", dirs_exist_ok=True
+                    )
+                else:
+                    shutil.copy(f"{self.path}/{file}", f"{outputDir}/{file}")
+
+        if os.path.exists(f"{self.path}/README.md"):
+            with open(f"{outputDir}/README.md", "a") as f:
+                f.write("\n\n")
+                f.write(self.config["description"])
+                f.write(open(f"{self.path}/README.md").read())
+
+        submodules = []
+        if "submodules" in self.config:
+            enabled = prompt(
+                {
+                    "name": "enabled",
+                    "message": "Submodules",
+                    "type": "checkbox",
+                    # TODO: need to show submodule description here
+                    "choices": map(lambda x: {"name": x}, self.config["submodules"]),
+                }
+            )["enabled"]
+
+            for s in enabled:
+                submodules.append(Module(f"{self.path}/{s}").execute(outputDir, False))
+
+        out = {
+            "name": self.config["name"],
+            "arguments": arguments,
+            "submodules": submodules,
+            "files": self.config["files"] if "files" in self.config else [],
+            "outputs": self.config["outputs"] if "outputs" in self.config else [],
+            "version": self.version,
+        }
+
+        if root:
+            with open(f"{outputDir}/.runxc.json", "a") as f:
+                f.write(json.dumps(out, indent=2))
+
+        return out
 
 
 @click.group()
@@ -25,139 +100,17 @@ def gen(overwrite: bool) -> int:
         "questions to generate your service."
     )
 
-    questions = [
-        {
-            "type": "input",
-            "name": "name",
-            "message": "Service name",
-            "default": "myservice",
-        },
-        {
-            "type": "input",
-            "name": "outputDir",
-            "message": "Output directory",
-            "default": lambda x: f"./{x['name']}",
-        },
-        {
-            "type": "list",
-            "name": "language",
-            "message": "Language",
-            "choices": ["python", "java", "go"],
-        },
-        {
-            "type": "list",
-            "name": "framework",
-            "message": "Framework",
-            "choices": ["flask", "django", "grpc"],
-        },
-        {
-            "type": "confirm",
-            "name": "deploy",
-            "message": "Kubernetes deploy template",
-            "default": True,
-        },
-        {
-            "type": "confirm",
-            "name": "infra",
-            "message": "Terraform infra template",
-            "default": True,
-        },
-        {
-            "type": "checkbox",
-            "name": "infra_config",
-            "message": "Terraform infra resources",
-            "choices": [
-                {"name": "K8s cluster"},
-                {"name": "Postgres database", "checked": True},
-            ],
-        },
-        {
-            "type": "input",
-            "name": "gcp_project",
-            "message": "GCP project",
-            "default": "catalog-289423",
-        },
-        {
-            "type": "input",
-            "name": "gcp_network",
-            "message": "GCP network",
-            "default": "projects/catalog-289423/global/networks/pg-private-network",
-        },
-        {
-            "type": "input",
-            "name": "terraform_bucket",
-            "message": "Terraform bucket",
-            "default": "runx_terraform_state",
-        },
-        {
-            "type": "input",
-            "name": "terraform_prefix",
-            "message": "Terraform prefix",
-            "default": lambda x: x["name"],
-        },
-    ]
-
-    answers = prompt(questions)
-    print(answers)
-    _generate(answers, overwrite)
-
-    print("Generated!")
-
-    return 0
-
-
-def _generate(answers: Mapping[str, Any], overwrite: bool) -> None:
-    if (
-        answers["language"] != "python"
-        or answers["framework"] != "flask"
-        or len(answers["infra_config"]) != 1
-        or answers["infra_config"][0] != "Postgres database"
-    ):
-        raise Exception("Not supported")
-
-    if os.path.exists(answers["outputDir"]):
-        if not overwrite:
-            raise Exception("Output dir already exists!")
-        else:
-            shutil.rmtree(answers["outputDir"])
-
     if TEMPLATE_DIR is None or not os.path.exists(TEMPLATE_DIR):
         raise Exception("Template dir doesn't exist!")
 
-    # Copy application
-    shutil.copytree(
-        f"{TEMPLATE_DIR}/application/{answers['language']}", f"{answers['outputDir']}/"
-    )
+    outputDir = prompt(
+        [{"type": "input", "name": "outputDir", "message": "Output directory"}]
+    )["outputDir"]
 
-    if answers["deploy"]:
-        # Copy deploy
-        shutil.copytree(f"{TEMPLATE_DIR}/deploy", f"{answers['outputDir']}/deploy")
+    Module(TEMPLATE_DIR).execute(outputDir)
+    print("Generated!")
 
-    if answers["infra"]:
-        # Copy infra
-        shutil.copytree(f"{TEMPLATE_DIR}/infra", f"{answers['outputDir']}/infra")
-
-    # Generate config
-    with open(f"{answers['outputDir']}/runxcconfig.json", "w") as f:
-        f.write(
-            json.dumps(
-                {
-                    "service_name": answers["name"],
-                    "language": answers["language"],
-                    "gen_config": answers,
-                    "cloud": "gcp",
-                    "gcp_project": answers["gcp_project"],
-                    "gcp_network": answers["gcp_network"],
-                    "terraform": {
-                        "bucket": answers["terraform_bucket"],
-                        "prefix": answers["terraform_prefix"],
-                    },
-                },
-                indent=2,
-            )
-        )
-
-    # TODO README
+    return 0
 
 
 if __name__ == "__main__":
