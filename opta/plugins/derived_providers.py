73a0c6672d3c8f5184c0fcce626e3160e797ef35
Ankur Dahiya <ankur@runx.dev>
2021-01-02 10:18:21 -0800

    doit

--- opta/plugins/derived_providers.py
+++ opta/plugins/derived_providers.py
@@ -1,21 +1,16 @@
-import json
-import os
 from typing import Any, Mapping
 
 from utils import deep_merge
 
 
 class DerivedProviders:
-    def __init__(self, env: Any, modules_dir: str):
+    def __init__(self, env: Any):
         self.env = env
-        self.modules_dir = modules_dir
 
     def gen_blocks(self) -> Mapping[Any, Any]:
         ret: Mapping[Any, Any] = {}
         for m in self.env.modules:
-            providers_file = f"{self.modules_dir}/{m.data['type']}/providers.json"
-            if os.path.exists(providers_file):
-                data = json.loads(open(providers_file).read())
-                ret = deep_merge(data, ret)
+            if "providers" in m.desc:
+                ret = deep_merge(m.desc["providers"], ret)
 
         return ret
