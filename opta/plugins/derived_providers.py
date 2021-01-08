c44979c63aa48557db2554d4c96fcd58c005fe75
JD <jdpcbs@gmail.com>
2021-01-08 15:03:24 -0800

    Made opta use providers from previous blocks

--- opta/plugins/derived_providers.py
+++ opta/plugins/derived_providers.py
@@ -4,19 +4,24 @@ from opta.utils import deep_merge, hydrate
 
 
 class DerivedProviders:
-    def __init__(self, layer: Any):
+    def __init__(self, layer: Any, is_parent: bool):
         self.layer = layer
+        self.is_parent = is_parent
 
     def gen_tf(self) -> Dict[Any, Any]:
         ret: Dict[Any, Any] = {}
         if self.layer is None:
             return ret
-        for b in self.layer.blocks:
+        blocks = self.layer.blocks if self.is_parent else self.layer.blocks[:-1]
+        for b in blocks:
             for m in b.modules:
                 if "providers" in m.desc:
-                    ret = deep_merge(m.desc["providers"], ret)
-        hydration = {
-            "layer_name": self.layer.meta["name"],
-            "state_storage": self.layer.state_storage(),
-        }
-        return hydrate(ret, hydration)
+                    hydration = {
+                        "layer_name": self.layer.meta["name"],
+                        "state_storage": self.layer.state_storage(),
+                        "module_source": "data.terraform_remote_state.parent.outputs"
+                        if self.is_parent
+                        else f"module.{m.key}",
+                    }
+                    ret = deep_merge(hydrate(m.desc["providers"], hydration), ret)
+        return ret
