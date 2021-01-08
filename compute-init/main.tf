ca660d97d07f668b33e1777402da01e1ae01a3ab
JD <jdpcbs@gmail.com>
2021-01-08 11:28:45 -0800

    Creating a module for node pools

--- compute-init/main.tf
+++ compute-init/main.tf
@@ -40,37 +40,3 @@ resource "google_container_cluster" "prod-main" {
   }
   network = google_compute_network.private-network.id
 }
-
-resource "google_container_node_pool" "prod-main-nodes" {
-  name               = var.name
-  cluster            = google_container_cluster.prod-main.name
-  initial_node_count = 1
-  management {
-    auto_repair  = "true"
-    auto_upgrade = "true"
-  }
-  node_config {
-    disk_size_gb = 100
-    disk_type    = "pd-standard"
-    image_type   = "COS"
-    machine_type = "e2-standard-4"
-    metadata = {
-      "disable-legacy-endpoints" = "true"
-    }
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-      "https://www.googleapis.com/auth/service.management.readonly",
-      "https://www.googleapis.com/auth/servicecontrol",
-      "https://www.googleapis.com/auth/trace.append",
-    ]
-    preemptible     = false
-    service_account = "default"
-
-    shielded_instance_config {
-      enable_integrity_monitoring = true
-      enable_secure_boot          = false
-    }
-  }
-}
