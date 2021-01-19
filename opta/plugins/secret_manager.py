7f52ff7268bfcdea550a2ac5b543fcada8749c73
Ankur Dahiya <ankur@runx.dev>
2021-01-19 15:13:21 -0800

    update command

--- opta/plugins/secret_manager.py
+++ opta/plugins/secret_manager.py
@@ -2,11 +2,11 @@ import base64
 import json
 import os
 import subprocess
-from typing import Optional
 
 import click
 
 from opta.layer import Layer
+from opta.module import Module
 from opta.utils import is_tool
 
 
@@ -15,64 +15,75 @@ def secret() -> None:
     pass
 
 
-def get_name_if_exists(secret: str, configfile: str) -> Optional[str]:
+def get_module(app: str, secret: str, configfile: str) -> Module:
     if not is_tool("kubectl"):
         raise Exception("Please install kubectl on your machine")
     if not os.path.exists(configfile):
         raise Exception(f"File {configfile} not found")
 
     layer = Layer.load_from_yaml(configfile)
+
+    target_module = None
     for block in layer.blocks:
         for module in block.modules:
-            if secret == module.key and module.data["type"] == "k8s-secret":
-                secret_name = f"{layer.meta['name']}.{module.key}"
-                return secret_name
+            if module.key == app:
+                if "secrets" in module.data and secret in module.data["secrets"]:
+                    target_module = module
+                    break
 
-    return None
-
-
-@secret.command()
-@click.option("--secret", required=True, help="Secret name")
-@click.option("--configfile", default="opta.yml", help="Opta config file")
-def view(secret: str, configfile: str) -> None:
-    secret_name = get_name_if_exists(secret, configfile)
-
-    if secret_name is not None:
-        output = subprocess.run(
-            ["kubectl", "get", f"secrets/{secret_name}", "--template={{.data.value}}"],
-            capture_output=True,
-        )
-        if output.returncode == 0:
-            print(base64.b64decode(output.stdout).decode("utf-8"))
-            return
-
-    raise Exception("Secret not found")
+    if not target_module:
+        raise Exception("Secret not found")
+    else:
+        return target_module
 
 
 @secret.command()
-@click.option("--secret", required=True, help="Secret name")
-@click.option("--value", required=True, help="Value")
+@click.argument("app")
+@click.argument("secret")
 @click.option("--configfile", default="opta.yml", help="Opta config file")
-def update(secret: str, value: str, configfile: str) -> None:
-    secret_name = get_name_if_exists(secret, configfile)
+def view(app: str, secret: str, configfile: str) -> None:
+    target_module = get_module(app, secret, configfile)
+
+    output = subprocess.run(
+        [
+            "kubectl",
+            "get",
+            "secrets/secret",
+            f"--namespace={target_module.name}",
+            f"--template={{{{.data.{secret}}}}}",
+        ],
+        capture_output=True,
+    )
+
+    if output.returncode != 0:
+        raise Exception("Something went wrong")
+
+    print(base64.b64decode(output.stdout).decode("utf-8"))
+
+
+@secret.command()
+@click.argument("app")
+@click.argument("secret")
+@click.argument("value")
+@click.option("--configfile", default="opta.yml", help="Opta config file")
+def update(app: str, secret: str, value: str, configfile: str) -> None:
+    target_module = get_module(app, secret, configfile)
     secret_value = base64.b64encode(value.encode("utf-8")).decode("utf-8")
-    patch = [{"op": "replace", "path": "/data/value", "value": secret_value}]
+    patch = [{"op": "replace", "path": f"/data/{secret}", "value": secret_value}]
 
-    if secret_name is not None:
-        ret = subprocess.run(
-            [
-                "kubectl",
-                "patch",
-                "secret",
-                secret_name,
-                "--type=json",
-                f"-p={json.dumps(patch)}",
-            ]
-        )
-        if ret.returncode == 0:
-            print("Success")
-            return
-        else:
-            raise Exception(ret)
+    ret = subprocess.run(
+        [
+            "kubectl",
+            "patch",
+            "secret",
+            "secret",
+            f"--namespace={target_module.name}",
+            "--type=json",
+            f"-p={json.dumps(patch)}",
+        ]
+    )
 
-    raise Exception("Secret not found")
+    if ret.returncode != 0:
+        raise Exception("Something went wrong")
+
+    print("Success")
