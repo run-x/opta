45dc8ebebeb9af910e9e3d338a3bc57a2c00abf0
Juan Diego Palomino <jdpcbs@gmail.com>
2021-01-22 12:02:38 -0800

    Some improvements for datadog deployment (#22)
    * Some improvements for datadog deployment
    
    1. Enable K8s event collection for nice display
    2. Enable logging
    3. Enable using datadog metrics for pod autoscaling
    4. Enable auto-injection of datadog env, service, and version as environment variables into the containers based on labels
    5. Add helm release as a datadog tag
    
    What will the effects to the user be once I add the labels as part of the helm charts? Zero
    
    * whoopsie

--- datadog/main.tf
+++ datadog/main.tf
@@ -7,14 +7,51 @@ terraform {
   }
 }
 
+resource "random_password" "cluster_agent_token" {
+  length = 16
+  special = false
+}
+
 resource "helm_release" "datadog" {
-  count = var.api_key == null ? 0 : 1
+  count = (var.api_key == null || var.api_key == "") ? 0 : 1
   repository = "https://helm.datadoghq.com"
   chart = "datadog"
   name = var.name
 
   values = [
-    file("${path.module}/values.yml")
+    yamlencode({
+      rbac: {
+        create: true
+      }
+      datadog: {
+        leaderElection: true
+        collectEvents: true
+
+        logs: {
+          enabled: true
+          containerCollectAll: true
+        }
+
+        apm: {
+          enabled: true
+        }
+        podLabelsAsTags: {
+          app: "kube_app"
+          release: "helm_release"
+        }
+      }
+      clusterAgent: {
+        enabled: true
+        token: random_password.cluster_agent_token.result
+        metricsProvider: {
+          enabled: true
+        }
+        admissionController: {
+          enabled: true
+          mutateUnlabelled: true
+        }
+      }
+    })
   ]
 
 
@@ -23,6 +60,8 @@ resource "helm_release" "datadog" {
     value = var.api_key
   }
 
+  namespace = "datadog"
+  create_namespace = true
   atomic          = true
   cleanup_on_fail = true
   recreate_pods   = true
