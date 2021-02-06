206176c6d6434e3e5e917abade3962e2e4423d63
Kevin Jin <kevin.jin@vanderbilt.edu>
2021-02-05 19:23:04 -0700

    add reconfigure flag

--- opta/output.py
+++ opta/output.py
@@ -7,7 +7,7 @@ from opta.nice_subprocess import nice_run  # noqa: E402
 def get_terraform_outputs(force_init: bool = False) -> str:
     """ Fetch terraform outputs from existing TF file """
     if force_init or not _terraform_dir_exists():
-        nice_run(["terraform", "init"], check=True)
+        nice_run(["terraform", "init", "-reconfigure"], check=True)
     nice_run(["terraform", "get", "--update"], check=True)
     return nice_run(
         ["terraform", "output", "-json"], check=True, capture_output=True
