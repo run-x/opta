795576203889a1cda1faff89f8836a5f1476ab28
Kevin Jin <kevin.jin@vanderbilt.edu>
2021-02-10 00:39:45 -0700

    refactor

--- opta/inspect_cmd.py
+++ opta/inspect_cmd.py
@@ -6,7 +6,7 @@ from typing import Any, List
 import yaml
 
 from opta.nice_subprocess import nice_run
-from opta.utils import is_tool
+from opta.utils import deep_merge, is_tool
 from opta.var import TF_FILE_PATH
 
 INSPECT_CONFIG = "inspect.yml"
@@ -16,8 +16,6 @@ def inspect_cmd() -> None:
     # Make sure the user has the prerequisite CLI tools installed
     if not is_tool("terraform"):
         raise Exception("Please install terraform on your machine")
-
-    aws_region = _get_aws_region()
 
     # Fetch the inspect config template
     inspect_config_file_path = os.path.join(os.path.dirname(__file__), INSPECT_CONFIG)
@@ -30,6 +28,7 @@ def inspect_cmd() -> None:
     for resource in resources:
         # Every terraform resource has an "address", and this is what
         # the inspect config matches on.
+        # Ex. "module.app.helm_release.k8s-service"
         resource_address = resource.get("address", "")
 
         # If the terraform resource address has "[0]" or "[#]" appending it, then
@@ -38,59 +37,66 @@ def inspect_cmd() -> None:
             continue
 
         for inspect_key in inspected_resource_mappings:
-            # For example, the inspect key may be "helm_release.k8s-service" and the full
-            # resource address is "module.app.helm_release.k8s-service".
+            # The inspect key should be a substring of the full resource address
+            # Ex. "helm_release.k8s-service" or "aws_docdb_cluster"
             if inspect_key in resource_address:
                 resource_name = inspected_resource_mappings[inspect_key].get("name") or ""
-                resource_description = (
-                    inspected_resource_mappings[inspect_key].get("desc") or ""
-                )
+                resource_desc = inspected_resource_mappings[inspect_key].get("desc") or ""
                 resource_template_url = (
                     inspected_resource_mappings[inspect_key].get("url") or ""
                 )
 
-                resource_values = {"aws_region": aws_region}
+                # Generate the resource url from the template.
+                template_url_values = _get_template_url_values(resource, inspect_key)
+                resource_url = resource_template_url.format(**template_url_values)
 
-                terraform_resource_values = resource.get("values")
+                inspect_details.append((resource_name, resource_desc, resource_url))
 
-                # Inspect key-specific logic
-                print(inspect_key, terraform_resource_values.keys())
-                if (
-                    inspect_key == "helm_release.k8s-service"
-                    and "metadata" in terraform_resource_values
-                ):
-                    k8s_metadata = terraform_resource_values["metadata"]
-                    resource_values = {
-                        **resource_values,
-                        **_get_k8s_metadata_values(k8s_metadata),
-                    }
-                    del terraform_resource_values["metadata"]
-
-                for k, v in terraform_resource_values.items():
-                    resource_values[k] = str(v)
-
-                resource_url = resource_template_url.format(**resource_values)
-                inspect_details.append(
-                    (resource_name, resource_description, resource_url)
-                )
+                # Only match a resource to its first matching inspect key.
                 break
 
+    # Sort the inspected resources alphabetically
     inspect_details.sort()
+    # Add columm headers to the displayed output
     inspect_details.insert(0, ("NAME", "DESCRIPTION", "LINK"))
     column_print(inspect_details)
 
 
-def _get_k8s_metadata_values(metadata: int):
-    k8s_values = {}
-    for chart in metadata:
-        chart_values = json.loads(chart.get("values", "{}"))
-        k8s_values = {**k8s_values, **chart_values}
+# Get values that may be needed to populate the resource's template URL.
+def _get_template_url_values(resource_state: dict, inspect_key: str) -> dict:
+    template_url_values = {}
 
-    values = {}
+    # The template url may require the current AWS region.
+    template_url_values["aws_region"] = _get_aws_region()
+
+    # Get the resource properties from the terraform state, which
+    # may be used to populate the template URL.
+    resource_properties = resource_state.get("values", {})
+    for k, v in resource_properties.items():
+        template_url_values[k] = str(v)
+
+    # Some inspect keys may require custom logic to fetch the values
+    # for their template URL.
+    if inspect_key == "helm_release.k8s-service":
+        k8s_metadata_values = _get_k8s_metadata_values(resource_properties)
+        template_url_values = deep_merge(template_url_values, k8s_metadata_values)
+
+    return template_url_values
+
+
+def _get_k8s_metadata_values(resource_properties: dict) -> dict:
+    if "metadata" not in resource_properties:
+        return {}
+
+    k8s_values: Any = {}
+    for chart in resource_properties["metadata"]:
+        chart_values = json.loads(chart.get("values", "{}"))
+        k8s_values = deep_merge(k8s_values, chart_values)
+
+    values: Any = {}
     for k, v in k8s_values.items():
         values[f"k8s-{k}"] = v
 
-    print(values.keys())
     return values
 
 
