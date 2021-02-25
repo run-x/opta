a20aff509cbb078833a86b55e6ec3022516e738c
Nitin Aggarwal <nitin.agg1909@gmail.com>
2021-02-25 11:55:45 -0800

    fix

--- opta/commands/secret.py
+++ opta/commands/secret.py
@@ -16,29 +16,17 @@ def secret() -> None:
     """Commands for manipulating secrets for a k8s service"""
     pass
 
-
-def get_module(module_name: str, env: Optional[str], config: str) -> Module:
-    layer = Layer.load_from_yaml(config, env)
-    target_module = layer.get_module(module_name)
-
-    if target_module is None:
-        raise UserErrors(f"Invalid target module {module_name}. Not found in layer")
-    else:
-        return target_module
-
-
 @secret.command()
-@click.argument("module_name")
 @click.argument("secret")
 @click.option("--env", default=None, help="The env to use when loading the config file")
 @click.option("--config", default="opta.yml", help="Opta config file", show_default=True)
 def view(module_name: str, secret: str, env: Optional[str], config: str) -> None:
     """View a given secret of a k8s service"""
-    target_module = get_module(module_name, env, config)
+    layer = Layer.load_from_yaml(config, env)
     amplitude_client.send_event(amplitude_client.VIEW_SECRET_EVENT)
     load_kube_config()
     v1 = CoreV1Api()
-    api_response = v1.read_namespaced_secret("secret", target_module.layer_name)
+    api_response = v1.read_namespaced_secret("secret", layer.name)
     if secret not in api_response.data:
         raise UserErrors(
             f"Secret {secret} was not specified for the app. You sure you set it in your yaml?"
@@ -53,12 +41,12 @@ def view(module_name: str, secret: str, env: Optional[str], config: str) -> None
 @click.option("--config", default="opta.yml", help="Opta config file", show_default=True)
 def list_command(module_name: str, env: Optional[str], config: str) -> None:
     """List the secrets setup for the given k8s service module"""
-    target_module = get_module(module_name, env, config)
+    layer = Layer.load_from_yaml(config, env)
     amplitude_client.send_event(amplitude_client.LIST_SECRETS_EVENT)
 
     load_kube_config()
     v1 = CoreV1Api()
-    api_response = v1.read_namespaced_secret("secret", target_module.layer_name)
+    api_response = v1.read_namespaced_secret("secret", layer.name)
 
     for key in api_response.data:
         print(key)
@@ -74,12 +62,12 @@ def update(
     module_name: str, secret: str, value: str, env: Optional[str], config: str
 ) -> None:
     """Update a given secret of a k8s service with a new value"""
-    target_module = get_module(module_name, env, config)
+    layer = Layer.load_from_yaml(config, env)
     amplitude_client.send_event(amplitude_client.UPDATE_SECRET_EVENT)
     secret_value = base64.b64encode(value.encode("utf-8")).decode("utf-8")
     patch = [{"op": "replace", "path": f"/data/{secret}", "value": secret_value}]
     load_kube_config()
     v1 = CoreV1Api()
-    v1.patch_namespaced_secret("secret", target_module.layer_name, patch)
+    v1.patch_namespaced_secret("secret", layer.name, patch)
 
     print("Success")
