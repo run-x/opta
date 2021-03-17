609e17ec2f1da4d0281204436d3b8c2cb2ff8dce
Juan Diego Palomino <jdpcbs@gmail.com>
2021-03-17 11:20:18 -0700

    Bugfixing missing psql encryption (#145)
    * Adding kms encryption at rest for aurora
    
    * Adding performance insights

--- config/tf_modules/aws-postgres/main.tf
+++ config/tf_modules/aws-postgres/main.tf
@@ -7,6 +7,10 @@ data "aws_security_group" "security_group" {
   name = "opta-${var.env_name}-db-sg"
 }
 
+data "aws_kms_key" "main" {
+  key_id = "alias/opta-${var.env_name}"
+}
+
 resource "aws_rds_cluster" "db_cluster" {
   cluster_identifier = "opta-${var.layer_name}-${var.module_name}"
   db_subnet_group_name = "opta-${var.env_name}"
@@ -19,6 +23,11 @@ resource "aws_rds_cluster" "db_cluster" {
   backup_retention_period = 5
   apply_immediately = true
   skip_final_snapshot = true
+  storage_encrypted = true
+  kms_key_id = data.aws_kms_key.main.arn
+  lifecycle {
+    ignore_changes = [storage_encrypted, kms_key_id]
+  }
 }
 
 resource "aws_rds_cluster_instance" "db_instance" {
@@ -30,4 +39,6 @@ resource "aws_rds_cluster_instance" "db_instance" {
   engine_version     = aws_rds_cluster.db_cluster.engine_version
   apply_immediately = true
   auto_minor_version_upgrade = false
+  performance_insights_enabled = true
+  performance_insights_kms_key_id = data.aws_kms_key.main.arn
 }
