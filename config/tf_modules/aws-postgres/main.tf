bc27141253bceea55732e535df9247d0002f0714
JD <jdpcbs@gmail.com>
2021-03-17 11:10:33 -0700

    Adding performance insights

--- config/tf_modules/aws-postgres/main.tf
+++ config/tf_modules/aws-postgres/main.tf
@@ -39,4 +39,6 @@ resource "aws_rds_cluster_instance" "db_instance" {
   engine_version     = aws_rds_cluster.db_cluster.engine_version
   apply_immediately = true
   auto_minor_version_upgrade = false
+  performance_insights_enabled = true
+  performance_insights_kms_key_id = data.aws_kms_key.main.arn
 }
