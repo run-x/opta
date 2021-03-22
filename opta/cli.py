47b5cf8c750ca648b45fd8d8fd3e97bd1e2a9e7d
Kevin Jin <kevin.jin@vanderbilt.edu>
2021-03-21 21:12:00 -0600

    add destroy command back in

--- opta/cli.py
+++ opta/cli.py
@@ -11,6 +11,7 @@ from opta.amplitude import amplitude_client
 from opta.commands.apply import apply
 from opta.commands.cleanup import cleanup
 from opta.commands.deploy import deploy
+from opta.commands.destroy import destroy
 from opta.commands.events import events
 from opta.commands.inspect_cmd import inspect
 from opta.commands.kubectl import configure_kubectl
@@ -72,6 +73,7 @@ def _cleanup() -> None:
 cli.add_command(apply)
 cli.add_command(cleanup)
 cli.add_command(deploy)
+cli.add_command(destroy)
 cli.add_command(configure_kubectl)
 cli.add_command(inspect)
 cli.add_command(logs)
