7c6685fe300fe5b86a4a0ac75921c849071236f3
Kevin Jin <kevin.jin@vanderbilt.edu>
2021-03-28 20:05:03 -0600

    delete hosted zone stuff'

--- opta/core/aws.py
+++ opta/core/aws.py
@@ -1,4 +1,3 @@
-from time import sleep
 from typing import TYPE_CHECKING
 
 import boto3
@@ -56,54 +55,6 @@ class AWS:
         s3_client = boto3.client("s3")
         s3_client.upload_file(config, bucket, config_path)
         logger.debug("Uploaded opta config to s3")
-
-    @staticmethod
-    def delete_hosted_zone(zone_id: str) -> None:
-        AWS._delete_hosted_zone_records(zone_id)
-
-        client = boto3.client("route53")
-        delete_state = client.delete_hosted_zone(Id=zone_id)
-        delete_status = AWS._wait_for_route53_delete_completion(delete_state)
-
-        print(f"Hosted zone ({zone_id}) deleted with status {delete_status}")
-
-    @staticmethod
-    # Before a hosted zone can be deleted, all of its non-required records must
-    # be removed.
-    def _delete_hosted_zone_records(zone_id: str) -> None:
-        client = boto3.client("route53")
-        # TODO: Pagination is necessary after 100+ records in a zone.
-        list_resp = client.list_resource_record_sets(HostedZoneId=zone_id)
-        records = list_resp["ResourceRecordSets"]
-
-        non_required_records = [
-            record for record in records if record["Type"] not in ["NS", "SOA"]
-        ]
-        if len(non_required_records) == 0:
-            return
-
-        delete_records_batch = list(
-            map(
-                lambda x: {"Action": "DELETE", "ResourceRecordSet": x},
-                non_required_records,
-            )
-        )
-        delete_state = client.change_resource_record_sets(
-            HostedZoneId=zone_id, ChangeBatch={"Changes": delete_records_batch}
-        )
-        delete_status = AWS._wait_for_route53_delete_completion(delete_state)
-
-        print(f"Records in hosted zone ({zone_id} deleted with status {delete_status}")
-
-    @staticmethod
-    def _wait_for_route53_delete_completion(delete_state: dict) -> str:
-        client = boto3.client("route53")
-        while delete_state["ChangeInfo"]["Status"] == "PENDING":
-            sleep(5)
-            logger.debug("AWS Route53 resource delete pending...")
-            delete_state = client.get_change(Id=delete_state["ChangeInfo"]["Id"])
-
-        return delete_state["ChangeInfo"]["Status"]
 
 
 # AWS Resource ARNs can be one of the following 3 formats:
