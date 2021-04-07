ec3777ea1bce21ba6642cc928685682d7a8185ae
JD <jdpcbs@gmail.com>
2021-04-07 02:14:01 -0700

    Added one more test

--- tests/module_processors/test_aws_dns.py
+++ tests/module_processors/test_aws_dns.py
@@ -1,13 +1,11 @@
 # type: ignore
 import os
 
-import pytest
 from pytest_mock import MockFixture
 
 from opta.layer import Layer
 from opta.module_processors.aws_dns import (
     CERTIFICATE_BODY_FILE_NAME,
-    CERTIFICATE_CHAIN_FILE_NAME,
     PRIVATE_KEY_FILE_NAME,
     AwsDnsProcessor,
 )
@@ -117,11 +115,6 @@ class TestAwsDnsProcessor:
             None,
         )
         dns_module = layer.get_module("awsdns", 6)
-        body_path = os.path.join(
-            os.path.dirname(os.path.dirname(__file__)),
-            "module_processors",
-            "dummy_cert.pem",
-        )
         mocked_ssm = mocker.Mock()
         mocked_boto3 = mocker.patch("opta.module_processors.aws_dns.boto3")
         mocked_boto3.client.return_value = mocked_ssm
@@ -134,3 +127,53 @@ class TestAwsDnsProcessor:
         processor = AwsDnsProcessor(dns_module, layer)
         processor.process(2)
         mocked_boto3.client.assert_called_once_with("ssm", config=mocker.ANY)
+
+    def test_process_new(self, mocker: MockFixture):
+        layer = Layer.load_from_yaml(
+            os.path.join(
+                os.path.dirname(os.path.dirname(__file__)),
+                "module_processors",
+                "dummy_config_parent.yaml",
+            ),
+            None,
+        )
+        dns_module = layer.get_module("awsdns", 6)
+        mocked_ssm = mocker.Mock()
+        mocked_boto3 = mocker.patch("opta.module_processors.aws_dns.boto3")
+        mocked_boto3.client.return_value = mocked_ssm
+        mocked_ssm.get_parameters_by_path.return_value = {"Parameters": []}
+
+        patched_prompt = mocker.patch("opta.module_processors.aws_dns.prompt")
+        patched_prompt.side_effect = [
+            os.path.join(
+                os.path.dirname(os.path.dirname(__file__)),
+                "module_processors",
+                "dummy_key.pem",
+            ),
+            os.path.join(
+                os.path.dirname(os.path.dirname(__file__)),
+                "module_processors",
+                "dummy_cert.pem",
+            ),
+            "",
+        ]
+
+        processor = AwsDnsProcessor(dns_module, layer)
+        processor.process(2)
+        mocked_boto3.client.assert_called_once_with("ssm", config=mocker.ANY)
+        mocked_ssm.put_parameter.assert_has_calls(
+            [
+                mocker.call(
+                    Name=mocker.ANY,
+                    Value=mocker.ANY,
+                    Type="SecureString",
+                    Overwrite=True,
+                ),
+                mocker.call(
+                    Name=mocker.ANY,
+                    Value=mocker.ANY,
+                    Type="SecureString",
+                    Overwrite=True,
+                ),
+            ]
+        )
