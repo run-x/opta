e062d3cb88cd582cc8b1d2f923102e7eae0cf516
Ankur Dahiya <legalos.lotr@gmail.com>
2021-04-23 04:13:51 -1000

    Use image tag for non-auto deploys (#221)

--- config/tf_modules/k8s-service/main.tf
+++ config/tf_modules/k8s-service/main.tf
@@ -29,7 +29,7 @@ resource "helm_release" "k8s-service" {
         memory: "${var.resource_request["memory"]}Mi"
       },
       deployPods: (var.image != "AUTO") || (var.tag != null),
-      image: var.image == "AUTO" ? (var.tag == null ? "": "${aws_ecr_repository.repo[0].repository_url}:${var.tag}") : var.image
+      image: var.image == "AUTO" ? (var.tag == null ? "": "${aws_ecr_repository.repo[0].repository_url}:${var.tag}") : (var.tag == null ? var.image : "${var.image}:${var.tag}")
       version: var.tag == null ? "latest" : var.tag
       livenessProbePath: var.healthcheck_path == null || var.liveness_probe_path != "/healthcheck"? var.liveness_probe_path : var.healthcheck_path,
       readinessProbePath: var.healthcheck_path == null || var.readiness_probe_path != "/healthcheck"? var.readiness_probe_path : var.healthcheck_path,
