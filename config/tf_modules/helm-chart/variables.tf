9ac2612191d9538e5a9f4d31e124af6cd18c7d74
JD <jdpcbs@gmail.com>
2021-04-28 17:23:50 -0700

    helm chart support

---
+++ config/tf_modules/helm-chart/variables.tf
@@ -0,0 +1,64 @@
+variable "repository" {
+  type = string
+  default = null
+}
+
+variable "chart" {
+  type = string
+}
+
+variable "namespace" {
+  type = string
+  default = "default"
+}
+
+variable "create_namespace" {
+  type = bool
+  default = false
+}
+
+variable "atomic" {
+  type = bool
+  default = true
+}
+
+variable "cleanup_on_fail" {
+  type = bool
+  default = true
+}
+
+variable "chart_version" {
+  type = string
+  default = null
+}
+
+variable "values_file" {
+  type = string
+  default = null
+}
+
+variable "values" {
+  type = map
+  default = {}
+}
+
+variable "env_name" {
+  description = "Env name"
+  type = string
+}
+
+variable "layer_name" {
+  description = "Layer name"
+  type        = string
+}
+
+variable "module_name" {
+  description = "Module name"
+  type = string
+}
+
+variable "timeout" {
+  type = number
+  default = 300
+}
+
---
+++ opta/module_processors/helm_chart.py
@@ -0,0 +1,26 @@
+from typing import TYPE_CHECKING
+
+from opta.exceptions import UserErrors
+from opta.module_processors.base import ModuleProcessor
+
+if TYPE_CHECKING:
+    from opta.layer import Layer
+    from opta.module import Module
+
+
+class HelmChartProcessor(ModuleProcessor):
+    def __init__(self, module: "Module", layer: "Layer"):
+        if module.data["type"] != "helm-chart":
+            raise Exception(
+                f"The module {module.name} was expected to be of type k8s service"
+            )
+        super(HelmChartProcessor, self).__init__(module, layer)
+
+    def process(self, module_idx: int) -> None:
+        if "version" in self.module.data:
+            self.module.data["helm_version"] = self.module.data["version"]
+        if "repository" in self.module.data and "version" not in self.module.data:
+            raise UserErrors(
+                "If you specify a remote repository you must give a version."
+            )
+        super(HelmChartProcessor, self).process(module_idx)
