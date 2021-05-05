5735dc6d6ce66ab8341b47cda2481d43da4a3ec6
JD <jdpcbs@gmail.com>
2021-05-05 16:35:01 -0700

    Debugging

--- opta/module_processors/k8s_base.py
+++ opta/module_processors/k8s_base.py
@@ -63,6 +63,7 @@ class K8sBaseProcessor(AWSK8sModuleProcessor):
             marker = output.get("NextMarker", "")
             load_balancers = output["LoadBalancers"]
             current_arns = [x["LoadBalancerArn"] for x in load_balancers]
+            arn_to_lb = {x["LoadBalancerArn"]: x for x in load_balancers}
             tag_descriptions = client.describe_tags(ResourceArns=current_arns)[
                 "TagDescriptions"
             ]
@@ -75,7 +76,7 @@ class K8sBaseProcessor(AWSK8sModuleProcessor):
                     and tag_dict.get("kubernetes.io/service-name")
                     == "ingress-nginx/ingress-nginx-controller"
                 ):
-                    return load_balancers[idx]
+                    return arn_to_lb[tag_description["ResourceArn"]]
 
             if marker == "":
                 break
