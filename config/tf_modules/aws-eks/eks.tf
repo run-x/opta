ccf7d23ea891ff7789a1d2ff6ad8985c58c5aa62
Juan Diego Palomino <jdpcbs@gmail.com>
2021-05-06 11:24:06 -0700

    ignore kms changes (#231)
    * ignore kms changes
    
    * adding comment

--- config/tf_modules/aws-eks/eks.tf
+++ config/tf_modules/aws-eks/eks.tf
@@ -35,7 +35,10 @@ resource "aws_eks_cluster" "cluster" {
   }
   lifecycle {
     ignore_changes = [
-      vpc_config[0].subnet_ids
+      # For some reason a data refresh appears randomly and even though the exact same data is going to be read,
+      # terraform panics and thinks it needs to recreate the whole cluster.
+      vpc_config[0].subnet_ids,
+      encryption_config[0].provider[0].key_arn
     ]
   }
 }
