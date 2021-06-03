143608a63cf04a4ee79ba648cebbc01bc8b0990d
JD <jdpcbs@gmail.com>
2021-06-03 16:27:43 -0700

    addressing cr

--- config/tf_modules/gcp-mysql/outputs.tf
+++ config/tf_modules/gcp-mysql/outputs.tf
@@ -1,3 +1,4 @@
+// This list of outputs needs to be the same as the ones for gcp-postgres and should match the ones at gcp_k8s_service.py:handle_rds_link()
 output "db_user" {
   value = google_sql_user.root.name
 }
