e36c441349494c92ae3cfcc61fc87c367dfdd33b
Quinn Ngo <quinn@runx.dev>
2021-06-25 11:20:28 -0700

    Add first service template

--- opta/commands/init.py
+++ opta/commands/init.py
@@ -1,5 +1,5 @@
 from os import path
-from typing import Optional
+from typing import Dict, Optional
 
 import click
 import yaml
@@ -7,8 +7,26 @@ from click_didyoumean import DYMGroup
 
 from opta.commands.init_templates.environment.aws.template import awsTemplate
 from opta.commands.init_templates.environment.gcp.template import gcpTemplate
+from opta.commands.init_templates.service.k8s.template import k8sServiceTemplate
+from opta.commands.init_templates.template import Template
 
 EXAMPLES_DIR = path.join(path.dirname(__file__), "init_templates")
+
+
+def _write_result(file_path: str, result: dict) -> None:
+    print("\n\n--------------------------------------------\n")
+    print(f"About to write to {file_path}:\n")
+
+    yaml_str = yaml.dump(result, sort_keys=False)
+    print(yaml_str)
+    print("--------------------------------------------")
+
+    ok = input("\n\nIs this OK? (yes) ") or "yes"
+    if ok == "no":
+        print("Aborted")
+        return
+    file = open(file_path, "w")
+    file.write(yaml_str)
 
 
 @click.group(cls=DYMGroup)
@@ -41,28 +59,60 @@ Press ^C at any time to quit.
     else:
         res = gcpTemplate.run()
 
-    print(f"\n\nAbout to write to {name}:\n")
+    _write_result(file_path=name, result=res)
 
-    yaml_str = yaml.dump(res, sort_keys=False)
-    print(yaml_str)
 
-    ok = input("\n\nIs this OK? (yes) ") or "yes"
-    if ok == "no":
-        print("Aborted")
-        return
-    file = open(name, "w")
-    file.write(yaml_str)
+SERVICE_TEMPLATES: Dict[str, Template] = {
+    "k8s": k8sServiceTemplate,
+}
 
 
 @init.command()
-@click.option(
-    "-t", "--template", default=None, help="The opta template you would like to use"
-)
+@click.argument("template_name", type=click.Choice(SERVICE_TEMPLATES.keys()))
 @click.option(
     "-n",
-    "--name",
+    "--file-name",
     default="opta.yaml",
     help="The name of the file that this command will output",
 )
-def service(template: Optional[str], name: Optional[str]) -> None:
-    print("initializing service file")
+@click.option(
+    "-e",
+    "--environment-file",
+    type=click.Path(exists=True),
+    help="The name of the opta environment you would like to deploy this service in",
+)
+def service(
+    template_name: Optional[str], file_name: str, environment_file: Optional[str]
+) -> None:
+    print(
+        """This utility will walk you through creating an opta configuration file.
+It only covers the minimal configuration necessary to get an opta service
+up and running. For instructions on how to further customize this yaml file, go
+to https://docs.opta.dev/.
+
+Press ^C at any time to quit.
+    """
+    )
+
+    env_dict: Optional[dict] = None
+
+    template = SERVICE_TEMPLATES["k8s"]
+    res = template.run()
+
+    try:
+        if environment_file:
+            with open(environment_file) as f:
+                env_dict = yaml.safe_load(f)
+            if env_dict:
+                res["environments"] = [
+                    {
+                        "name": env_dict["name"],
+                        "path": path.relpath(environment_file),
+                        "variables": {},
+                    }
+                ]
+    except Exception:
+        print("Unable to find or process environment file. Ignoring for now...")
+
+    _write_result(file_name, res)
+    pass
--- opta/commands/init_templates/variables/dns_domain.py
+++ opta/commands/init_templates/variables/dns_domain.py
@@ -1,10 +1,6 @@
+from opta.commands.init_templates.helpers import set_module_field
 from opta.commands.init_templates.template import TemplateVariable
 
-
-def apply(d: dict, domain: str) -> dict:
-    [dns_module] = [module for module in d["modules"] if module["type"] == "dns"]
-    dns_module["domain"] = domain
-    return d
-
-
-dnsDomainVariable = TemplateVariable(prompt="dns domain", applier=apply,)
+dnsDomainVariable = TemplateVariable(
+    prompt="dns domain", applier=set_module_field("dns", ["domain"]),
+)
