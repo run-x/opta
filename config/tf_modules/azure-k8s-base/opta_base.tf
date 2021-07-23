ea7d3cac637074a7d026d8104a2f8fcddbeaa07d
JD <jdpcbs@gmail.com>
2021-07-23 01:26:53 -0700

    cleanup

--- config/tf_modules/azure-k8s-base/opta_base.tf
+++ config/tf_modules/azure-k8s-base/opta_base.tf
@@ -3,7 +3,6 @@ resource "helm_release" "opta_base" {
   name      = "opta-base"
   namespace = "default"
   values = [yamlencode({
-    blah: 1
     tls_key : base64encode(var.private_key),
     tls_crt : base64encode(join("\n", [var.certificate_body, var.certificate_chain]))
   })]
