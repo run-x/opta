b4f80804671f575dbdfec7414b42a2513d2497f6
JD <jdpcbs@gmail.com>
2021-08-12 21:20:02 -0700

    terraform fmt

--- config/tf_modules/k8s-base/nvidia_daemonset.tf
+++ config/tf_modules/k8s-base/nvidia_daemonset.tf
@@ -1,14 +1,14 @@
 resource "helm_release" "nvidia_daemonset" {
-  chart            = "nvidia-device-plugin"
-  name             = "nvidia-device-plugin"
-  repository       = "https://nvidia.github.io/k8s-device-plugin"
-  version          = "0.9.0"
-  atomic           = true
-  cleanup_on_fail  = true
+  chart           = "nvidia-device-plugin"
+  name            = "nvidia-device-plugin"
+  repository      = "https://nvidia.github.io/k8s-device-plugin"
+  version         = "0.9.0"
+  atomic          = true
+  cleanup_on_fail = true
   values = [
     yamlencode({
-      nodeSelector: {
-        gpu: "true"
+      nodeSelector : {
+        gpu : "true"
       }
     })
   ]
