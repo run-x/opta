71c5af0d4f8fce24324340ff3f448a6ac8ef72a2
JD <jdpcbs@gmail.com>
2021-08-27 15:27:49 -0700

    wip

---
+++ scripts/docs.py
@@ -0,0 +1,12 @@
+import argparse
+
+from opta.core.registry import make_registry_docs
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("docs")
+    parser.add_argument(
+        "directory", type=str, help="Root directory to add the auto-generated docs in",
+    )
+    args = parser.parse_args()
+
+    make_registry_docs(args.directory)
