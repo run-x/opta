37e4b8591e8a332d230402358ffa3a4695bf29d3
Nilesh Sarupriya <sarupriyanilesh@outlook.com>
2021-09-28 02:31:38 +0530

    Added retention limit & snapshot window. (#376)
    * Added retention limit & snapshot window.
    
    * Default to a time for Snapshot window

--- config/tf_modules/aws-redis/variables.tf
+++ config/tf_modules/aws-redis/variables.tf
@@ -22,3 +22,15 @@ variable "redis_version" {
   type    = string
   default = "6.x"
 }
+
+variable "snapshot_window" {
+  description = "When should the Snapshot for redis cache be done. UTC Time. Snapshot Retention Limit should be set to more than 0."
+  type        = string
+  default     = "04:00-05:00"
+}
+
+variable "snapshot_retention_limit" {
+  description = "Days for which the Snapshot should be retained."
+  type        = number
+  default     = 0
+}
