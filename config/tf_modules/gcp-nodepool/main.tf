0049b35cfccf5accb90ee671881427cd381cafa4
Nilesh Sarupriya <sarupriyanilesh@outlook.com>
2021-10-06 20:48:29 +0530

    Add IAM Member permissions

--- config/tf_modules/gcp-nodepool/main.tf
+++ config/tf_modules/gcp-nodepool/main.tf
@@ -18,6 +18,35 @@ resource "google_service_account" "gke_node" {
   display_name = "opta-${var.layer_name}-default-node-pool"
 }
 
+resource "google_project_iam_member" "gke_node_log_writer" {
+  role   = "roles/logging.logWriter"
+  member = "serviceAccount:${google_service_account.gke_node.email}"
+}
+
+resource "google_project_iam_member" "gke_node_metric_writer" {
+  role   = "roles/monitoring.metricWriter"
+  member = "serviceAccount:${google_service_account.gke_node.email}"
+}
+
+resource "google_project_iam_member" "gke_node_monitoring_viewer" {
+  role   = "roles/monitoring.viewer"
+  member = "serviceAccount:${google_service_account.gke_node.email}"
+}
+
+resource "google_project_iam_member" "gke_node_stackdriver_writer" {
+  role   = "roles/stackdriver.resourceMetadata.writer"
+  member = "serviceAccount:${google_service_account.gke_node.email}"
+}
+
+resource "google_storage_bucket_iam_member" "viewer" {
+  bucket = "artifacts.${data.google_client_config.current.project}.appspot.com"
+  role   = "roles/storage.objectViewer"
+  member = "serviceAccount:${google_service_account.gke_node.email}"
+  lifecycle {
+    ignore_changes = [bucket]
+  }
+}
+
 resource "google_container_node_pool" "node_pool" {
   name               = "opta-${var.layer_name}-${random_string.node_pool_id.id}"
   cluster            = data.google_container_cluster.main.name
