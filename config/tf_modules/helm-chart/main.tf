2ec317898232152c986eb7162320857b0aa802f3
JD <jdpcbs@gmail.com>
2021-10-06 10:08:14 -0700

    Supporting a list of helm values files
    Also, now we check for relative path and support it

--- config/tf_modules/helm-chart/main.tf
+++ config/tf_modules/helm-chart/main.tf
@@ -1,17 +1,14 @@
 resource "helm_release" "remote_chart" {
-  count            = var.repository == null ? 0 : 1
-  chart            = var.chart
-  repository       = var.repository
-  version          = var.chart_version
-  name             = "${var.layer_name}-${var.module_name}"
-  atomic           = var.atomic
-  cleanup_on_fail  = var.cleanup_on_fail
-  namespace        = var.namespace
-  create_namespace = var.create_namespace
-  values = var.values_file == null ? [yamlencode(var.values)] : [
-    file(var.values_file),
-    yamlencode(var.values)
-  ]
+  count             = var.repository == null ? 0 : 1
+  chart             = var.chart
+  repository        = var.repository
+  version           = var.chart_version
+  name              = "${var.layer_name}-${var.module_name}"
+  atomic            = var.atomic
+  cleanup_on_fail   = var.cleanup_on_fail
+  namespace         = var.namespace
+  create_namespace  = var.create_namespace
+  values            = var.values_files == [] ? [yamlencode(var.values)] : concat(local.values_from_files, [yamlencode(var.values)])
   timeout           = var.timeout
   dependency_update = var.dependency_update
   wait              = var.wait
@@ -19,17 +16,14 @@ resource "helm_release" "remote_chart" {
 
 
 resource "helm_release" "local_chart" {
-  count            = var.repository == null ? 1 : 0
-  chart            = var.chart
-  name             = "${var.layer_name}-${var.module_name}"
-  atomic           = var.atomic
-  cleanup_on_fail  = var.cleanup_on_fail
-  namespace        = var.namespace
-  create_namespace = var.create_namespace
-  values = var.values_file == null ? [yamlencode(var.values)] : [
-    file(var.values_file),
-    yamlencode(var.values)
-  ]
+  count             = var.repository == null ? 1 : 0
+  chart             = var.chart
+  name              = "${var.layer_name}-${var.module_name}"
+  atomic            = var.atomic
+  cleanup_on_fail   = var.cleanup_on_fail
+  namespace         = var.namespace
+  create_namespace  = var.create_namespace
+  values            = var.values_files == [] ? [yamlencode(var.values)] : concat(local.values_from_files, [yamlencode(var.values)])
   timeout           = var.timeout
   dependency_update = var.dependency_update
 }
