3dfdccc904fbf378d6b8db0b70acd41bc1c9997e
JD <jdpcbs@gmail.com>
2021-11-01 12:53:21 -0700

    minor bugfix

--- config/tf_modules/k8s-base/variables.tf
+++ config/tf_modules/k8s-base/variables.tf
@@ -3,10 +3,10 @@ data "aws_region" "current" {}
 locals {
   target_ports    = var.cert_arn == "" && var.private_key == "" ? { http : "http" } : { http : "http", https : "https" }
   container_ports = { http : 80, https : 443 }
-  nginx_tls_ports = var.cert_arn == "" && var.private_key == "" ? "" : join(",", compact(flatten(
+  nginx_tls_ports = var.cert_arn == "" && var.private_key == "" ? "" : join(",", compact(flatten([
     ["https"],
     [for port in var.nginx_extra_tcp_ports_tls : "${port}-tcp"],
-  )))
+  ])))
 
   config = merge((var.cert_arn == "" && var.private_key == "" ? { ssl-redirect : false } : {
     ssl-redirect : true
