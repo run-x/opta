f9c65a619f68d7a8e40566d4c4fa3ccdea423184
JD <jdpcbs@gmail.com>
2021-11-01 15:13:31 -0700

    bugfixing

--- opta/module_processors/aws_k8s_base.py
+++ opta/module_processors/aws_k8s_base.py
@@ -45,6 +45,8 @@ class AwsK8sBaseProcessor(AWSK8sModuleProcessor, K8sBaseModuleProcessor):
                 "certificate_chain"
             ] = f"${{{{module.{byo_cert_module.name}.certificate_chain}}}}"
 
+        Terraform.download_state(self.layer)
+        configure_kubectl(self.layer)
         self._process_nginx_extra_ports(self.module.data)
 
         aws_dns_module = None
