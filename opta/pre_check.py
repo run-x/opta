36eff16ca580171050bd567a99d4619dbeb73d92
Nilesh Sarupriya <sarupriyanilesh@outlook.com>
2021-11-05 23:09:24 +0530

    Use system shell to get consistent result

--- opta/pre_check.py
+++ opta/pre_check.py
@@ -15,7 +15,9 @@ def pre_check() -> None:
 
 
 def is_symlinked_path() -> Tuple[bool, str]:
-    pwd_path = nice_run(["pwd"], capture_output=True).stdout.decode("utf-8").strip()
+    pwd_path = (
+        nice_run(["pwd"], capture_output=True, shell=True).stdout.decode("utf-8").strip()
+    )
     cwd_path = os.getcwd()
 
     return pwd_path != cwd_path, cwd_path
