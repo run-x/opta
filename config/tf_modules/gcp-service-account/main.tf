bf3c2bcc1de99820aae403f208d64f958968d41d
Juan Diego Palomino <jdpcbs@gmail.com>
2021-11-12 11:42:25 -0800

    Added missing storage bucket get role need for gcs (#482)
    * Added missing storage bucket get role need for gcs
    
    If you can read/write to buckets you ought to be able to "get" the buckets (i.e. their metadata).
    
    Needed for flyte gcp
    
    * terraform fmt
    
    * added for gcp k8s service too
    
    * terraform fmt
    
    * Update config/tf_modules/gcp-service-account/variables.tf
    
    Co-authored-by: Patrick Fiedler <rabbitfang@users.noreply.github.com>
    
    * Update config/tf_modules/gcp-service-account/main.tf
    
    Co-authored-by: Patrick Fiedler <rabbitfang@users.noreply.github.com>
    
    * Update config/tf_modules/gcp-k8s-service/variables.tf
    
    Co-authored-by: Patrick Fiedler <rabbitfang@users.noreply.github.com>
    
    * Update config/tf_modules/gcp-k8s-service/iam.tf
    
    Co-authored-by: Patrick Fiedler <rabbitfang@users.noreply.github.com>
    
    Co-authored-by: Patrick Fiedler <rabbitfang@users.noreply.github.com>

--- config/tf_modules/gcp-service-account/main.tf
+++ config/tf_modules/gcp-service-account/main.tf
@@ -11,6 +11,13 @@ resource "google_service_account_iam_binding" "trust_k8s_workload_idu" {
 
 }
 
+resource "google_storage_bucket_iam_member" "bucket_get" {
+  for_each = local.get_buckets
+  bucket   = each.key
+  role     = "roles/storage.legacyBucketReader"
+  member   = "serviceAccount:${google_service_account.k8s_service.email}"
+}
+
 resource "google_storage_bucket_iam_member" "bucket_viewer" {
   count  = length(var.read_buckets)
   bucket = var.read_buckets[count.index]
