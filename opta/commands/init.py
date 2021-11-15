7c8f563383b7b48f59b8edfd21d53d3e0554e18e
Nilesh Sarupriya <sarupriyanilesh@outlook.com>
2021-11-16 03:12:20 +0530

    Update init.py

--- opta/commands/init.py
+++ opta/commands/init.py
@@ -6,11 +6,11 @@ import click
 @click.command()
 def init() -> None:
     """
-    This command brings to you an interactive way of creating an opta configuration file.
+    This command brings to you an interactive way of creating an opta configuration files.
     You can choose to create an environment configuration or a service file by using the command.
-
-    You will now be redirected to Opta's Yaml Generation UI. If the browser doesn't open, please open the Link.
-
-    `https://app.runx.dev/yaml-generator`
     """
+    print(
+        "You will now be redirected to Opta's Yaml Generation UI. If the browser doesn't open, please follow the link: "
+        "`https://app.runx.dev/yaml-generator`"
+    )
     webbrowser.open("https://app.runx.dev/yaml-generator")
