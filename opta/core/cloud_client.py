4f7021979e234f600b95984d0633d2cb4c74a025
Juan Diego Palomino <jdpcbs@gmail.com>
2021-11-15 11:53:29 -0800

    Update opta/core/cloud_client.py
    Co-authored-by: Patrick Fiedler <rabbitfang@users.noreply.github.com>

--- opta/core/cloud_client.py
+++ opta/core/cloud_client.py
@@ -8,17 +8,22 @@ class CloudClient:
     def __init__(self, layer: "Layer"):
         self.layer = layer
 
+    @abstractmethod
     def get_remote_config(self) -> Optional["StructuredConfig"]:
         pass
 
+    @abstractmethod
     def upload_opta_config(self) -> None:
         pass
 
+    @abstractmethod
     def delete_opta_config(self) -> None:
         pass
 
+    @abstractmethod
     def delete_remote_state(self) -> None:
         pass
 
+    @abstractmethod
     def get_terraform_lock_id(self) -> str:
-        pass
+        raise NotImplementedError()
