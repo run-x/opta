7c66b3a8c99e57b74fdd9111dcce651281f7c9a3
Nilesh Sarupriya <sarupriyanilesh@outlook.com>
2021-11-17 00:50:55 +0530

    Updated comment.

--- opta/commands/init.py
+++ opta/commands/init.py
@@ -6,8 +6,7 @@ import click
 @click.command()
 def init() -> None:
     """
-    Opens the interactive UI
-    You can choose to create an environment configuration or a service file by using the command.
+    Opens the interactive UI to create configuration files.
     """
     print(
         "You will now be redirected to Opta's Yaml Generation UI. If the browser doesn't open, please follow the link: "
