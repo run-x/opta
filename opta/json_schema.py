e43d05c2e807366121f62da46e499443237a304d
Quinn Ngo <quinn@runx.dev>
2021-11-17 10:34:12 -0800

    improve script errors

--- opta/json_schema.py
+++ opta/json_schema.py
@@ -1,7 +1,7 @@
 import json
 from copy import deepcopy
 from os import listdir
-from os.path import dirname, isfile, join
+from os.path import dirname, exists, isfile, join
 
 from ruamel.yaml import YAML
 
@@ -37,30 +37,45 @@ def check_json_schema(write: bool = False) -> None:
                 open(join(module_path, f"{module_name}.yaml"))
             )
             json_schema_file_path = join(module_schemas_path, f"{module_name}.json")
+
+            if not exists(json_schema_file_path):
+                raise Exception(
+                    f"No JSON Schema file detected for module {module_name}. Please create a file named {module_name}.json in the config/registry/schemas/modules"
+                )
+
             json_schema_file = open(json_schema_file_path)
             json_schema = json.load(json_schema_file)
 
             new_json_schema = deepcopy(json_schema)
 
-            if "properties" in json_schema:
-                module_inputs = module_registry_dict["inputs"]
-                for i in module_registry_dict["inputs"]:
-                    if i["user_facing"]:
-                        input_name = i["name"]
-                        if input_name not in json_schema["properties"]:
-                            raise Exception(
-                                f"property {input_name} is missing from json schema for module {module_name}"
-                            )
-                        input_property_dict = json_schema["properties"][input_name]
-                        input_property_dict["description"] = i["description"]
-                        if "default" in i:
-                            input_property_dict["default"] = i["default"]
-
-                new_json_schema["required"] = [
-                    i["name"]
-                    for i in module_inputs
-                    if i["user_facing"] and "required=True" in i["validator"]
+            REQUIRED_FIELDS = ["description", "properties"]
+            if "properties" not in json_schema:
+                missing_fields = [
+                    field for field in REQUIRED_FIELDS if field not in json_schema
                 ]
+                raise Exception(
+                    f"JSON schema at {json_schema_file_path} missing {missing_fields} field(s)"
+                )
+
+            module_inputs = module_registry_dict["inputs"]
+            for i in module_registry_dict["inputs"]:
+                if i["user_facing"]:
+                    input_name = i["name"]
+                    if input_name not in json_schema["properties"]:
+                        raise Exception(
+                            f"property {input_name} is missing from json schema for module {module_name}"
+                        )
+                    new_input_property_dict = new_json_schema["properties"][input_name]
+
+                    new_input_property_dict["description"] = i["description"]
+                    if "default" in i:
+                        new_input_property_dict["default"] = i["default"]
+
+            new_json_schema["required"] = [
+                i["name"]
+                for i in module_inputs
+                if i["user_facing"] and "required=True" in i["validator"]
+            ]
 
             new_json_schema["opta_metadata"] = {
                 "module_type": "environment"
