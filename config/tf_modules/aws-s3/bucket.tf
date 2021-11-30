2e848bc877a5f771627fd937ed636d0eb094f58e
JD <jdpcbs@gmail.com>
2021-11-30 08:08:51 -0800

    linting

--- config/tf_modules/aws-s3/bucket.tf
+++ config/tf_modules/aws-s3/bucket.tf
@@ -96,7 +96,7 @@ resource "aws_s3_bucket_public_access_block" "block" {
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
-  depends_on = [aws_s3_bucket_policy.policy]
+  depends_on              = [aws_s3_bucket_policy.policy]
 }
 
 resource "aws_s3_bucket_policy" "policy" {
