d7b198ae63d0e0be955698dd0999b67195a54c5b
JD <jdpcbs@gmail.com>
2021-12-06 00:02:26 -0800

    wip

--- modules/gcp_postgres/tf_module/main.tf
+++ modules/gcp_postgres/tf_module/main.tf
@@ -62,11 +62,17 @@ resource "google_sql_database" "main" {
   name     = "opta-${var.layer_name}-${var.module_name}-${random_id.key_suffix.hex}"
 }
 
+resource "time_sleep" "wait_30_seconds" {
+  depends_on = [google_sql_database.main]
+
+  destroy_duration = "30s"
+}
+
 resource "google_sql_user" "root" {
   instance = google_sql_database_instance.instance.name
   name     = "postgres"
   password = random_password.root_auth.result
 
   deletion_policy = "ABANDON"
-  depends_on      = [google_sql_database.main]
+  depends_on      = [time_sleep.wait_30_seconds]
 }
