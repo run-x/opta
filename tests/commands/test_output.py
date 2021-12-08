d4e5dac024ca300eb64a233ed378eed8ca313d1a
JD <jdpcbs@gmail.com>
2021-12-08 13:02:43 -0800

    test fixing

--- tests/commands/test_output.py
+++ tests/commands/test_output.py
@@ -58,7 +58,7 @@ def test_output_aws(mocker: MockFixture) -> None:
     mocker.patch(
         "opta.core.terraform.Terraform.get_outputs", return_value=TERRAFORM_OUTPUTS
     )
-    mocker.patch("opta.commands.output._load_extra_aws_outputs", wraps=lambda x, y: y)
+    mocker.patch("opta.commands.output._load_extra_aws_outputs", wraps=lambda y: y)
 
     runner = CliRunner()
     result = runner.invoke(output)
@@ -91,7 +91,7 @@ def test_output_gcp(mocker: MockFixture) -> None:
     mocker.patch(
         "opta.core.terraform.Terraform.get_outputs", return_value=TERRAFORM_OUTPUTS
     )
-    mocker.patch("opta.commands.output._load_extra_gcp_outputs", wraps=lambda x, y: y)
+    mocker.patch("opta.commands.output._load_extra_gcp_outputs", wraps=lambda y: y)
 
     runner = CliRunner()
     result = runner.invoke(output)
@@ -114,52 +114,16 @@ def test_load_extra_aws_outputs(mocker: MockFixture) -> None:
     mocked_root = mocker.Mock()
     mocked_layer.root.return_value = mocked_root
     mocked_root.name = "baloney"
-    mocked_boto3 = mocker.patch("opta.commands.output.boto3")
-    elb_client = mocker.Mock()
-    mocked_boto3.client.return_value = elb_client
-    elb_client.describe_load_balancers.return_value = {
-        "NextMarker": "",
-        "LoadBalancers": [
-            {"LoadBalancerArn": "abc"},
-            {"LoadBalancerArn": "def", "DNSName": "ghi"},
-        ],
-    }
-    elb_client.describe_tags.return_value = {
-        "TagDescriptions": [
-            {
-                "ResourceArn": "abc",
-                "Tags": [
-                    {"Key": "kubernetes.io/cluster/malarkey", "Value": "owned"},
-                    {
-                        "Key": "kubernetes.io/service-name",
-                        "Value": "ingress-nginx/ingress-nginx-controller",
-                    },
-                ],
-            },
-            {
-                "ResourceArn": "def",
-                "Tags": [
-                    {
-                        "Key": "kubernetes.io/service-name",
-                        "Value": "ingress-nginx/ingress-nginx-controller",
-                    },
-                    {"Key": "kubernetes.io/cluster/opta-baloney", "Value": "owned"},
-                ],
-            },
-        ]
-    }
-    assert _load_extra_aws_outputs(mocked_layer, current_outputs={}) == {
+    assert _load_extra_aws_outputs(current_outputs={"load_balancer_raw_dns": "ghi"}) == {
         "load_balancer_raw_dns": "ghi"
     }
-    elb_client.describe_load_balancers.assert_called_once_with(Marker="")
-    elb_client.describe_tags.assert_called_once_with(ResourceArns=["abc", "def"])
 
 
 def test_load_extra_gcp_outputs(mocker: MockFixture) -> None:
-    assert _load_extra_gcp_outputs(mocker.Mock(), {}) == {}
-    assert _load_extra_gcp_outputs(
-        mocker.Mock(), {"load_balancer_raw_ip": "1.2.3.4"}
-    ) == {"load_balancer_raw_ip": "1.2.3.4"}
-    assert _load_extra_gcp_outputs(
-        mocker.Mock(), {"parent.load_balancer_raw_ip": "1.2.3.4"}
-    ) == {"load_balancer_raw_ip": "1.2.3.4"}
+    assert _load_extra_gcp_outputs({}) == {}
+    assert _load_extra_gcp_outputs({"load_balancer_raw_ip": "1.2.3.4"}) == {
+        "load_balancer_raw_ip": "1.2.3.4"
+    }
+    assert _load_extra_gcp_outputs({"parent.load_balancer_raw_ip": "1.2.3.4"}) == {
+        "load_balancer_raw_ip": "1.2.3.4"
+    }
