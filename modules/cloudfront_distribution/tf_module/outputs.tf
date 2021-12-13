9223372f74166853c6d9b0eb7f3f3137d08a9150
Nilesh Sarupriya <sarupriyanilesh@outlook.com>
2021-12-14 03:54:25 +0530

    Updating the Count for Cloudfront Distribution.

--- modules/cloudfront_distribution/tf_module/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "cloudfront_domain" {
-  value = var.bucket_name != "" ? aws_cloudfront_distribution.s3_distribution[0].domain_name : aws_cloudfront_distribution.lb_distribution[0].domain_name
-}
