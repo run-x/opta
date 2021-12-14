3c3f3288ae32a2bcc496330a2f351690d4ed9616
Nilesh Sarupriya <sarupriyanilesh@outlook.com>
2021-12-14 12:07:08 +0000

    Change the Output domain name value

--- modules/cloudfront_distribution/tf_module/outputs.tf
+++ modules/cloudfront_distribution/tf_module/outputs.tf
@@ -1,3 +1,3 @@
 output "cloudfront_domain" {
-  value = var.bucket_name != "" ? aws_cloudfront_distribution.s3_distribution[0].domain_name : aws_cloudfront_distribution.lb_distribution[0].domain_name
+  value = var.s3_load_balancer_enabled == true ? aws_cloudfront_distribution.s3_distribution[0].domain_name : (var.eks_load_balancer_enabled == true ? aws_cloudfront_distribution.lb_distribution[0].domain_name : "")
 }
