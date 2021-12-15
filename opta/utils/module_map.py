30408329b40972a04c1da6582558fb61a4617624
Sachin Agarwal <sachin@bigbitbus.com>
2021-12-15 09:26:50 -0500

    Added some mapping code

---
+++ opta/utils/module_map.py
@@ -0,0 +1,48 @@
+import importlib
+from typing import Dict
+
+from modules.base import ModuleProcessor
+
+PROCESSOR_DICT: Dict[str, str] = {
+    "aws-k8s-service": "AwsK8sServiceProcessor",
+    "aws-k8s-base": "AwsK8sBaseProcessor",
+    "datadog": "DatadogProcessor",
+    "gcp-k8s-base": "GcpK8sBaseProcessor",
+    "gcp-k8s-service": "GcpK8sServiceProcessor",
+    "gcp-gke": "GcpGkeProcessor",
+    "aws-dns": "AwsDnsProcessor",
+    "aws-documentdb": "AwsDocumentDbProcessor",
+    "runx": "RunxProcessor",
+    "helm-chart": "HelmChartProcessor",
+    "aws-iam-role": "AwsIamRoleProcessor",
+    "aws-iam-user": "AwsIamUserProcessor",
+    "aws-eks": "AwsEksProcessor",
+    "aws-ses": "AwsEmailProcessor",
+    "aws-sqs": "AwsSqsProcessor",
+    "aws-sns": "AwsSnsProcessor",
+    "azure-base": "AzureBaseProcessor",
+    "azure-k8s-base": "AzureK8sBaseProcessor",
+    "azure-k8s-service": "AzureK8sServiceProcessor",
+    "local-k8s-service": "LocalK8sServiceProcessor",
+    "external-ssl-cert": "ExternalSSLCert",
+    "aws-s3": "AwsS3Processor",
+    "gcp-dns": "GCPDnsProcessor",
+    "gcp-service-account": "GcpServiceAccountProcessor",
+    "custom-terraform": "CustomTerraformProcessor",
+    "aws-dynamodb": "AwsDynamodbProcessor",
+    "mongodb-atlas": "MongodbAtlasProcessor",
+    "cloudfront-distribution": "AwsCloudfrontDstributionProcessor",
+    "lambda-function": "LambdaFunctionProcessor",
+}
+
+# Relies on the python file being module_some_name.py, and then 
+# the opta module file being named module-some-name
+def generate_pymodule_path(module_type: str) -> str:
+    base_name = "_".join(module_type.split("-"))
+    return ".".join(["module", base_name, base_name])
+
+
+def get_processor_class(module_type: str) -> ModuleProcessor:
+    pymodule_path = generate_pymodule_path(module_type)
+    pymodule = importlib.import_module(pymodule_path)
+    return pymodule[PROCESSOR_DICT[module_type]]
---
+++ tests/mock_module_map.py
@@ -0,0 +1,25 @@
+from modules.base import ModuleProcessor
+import pytest
+from pytest_mock import MockFixture as mocker
+from opta.layer import Layer
+def get_processor_class(module_type: str) -> ModuleProcessor:
+        mocked_datadog_processor = mocker.patch("opta.layer.DatadogProcessor")
+        mock_dict = {}
+        mock_dict["datadog"] = mocked_datadog_processor
+        mocked_k8s_base_processor = mocker.patch("opta.layer.AwsK8sBaseProcessor")
+        mock_dict["aws-k8s-base"] = mocked_k8s_base_processor
+        mocked_eks_processor = mocker.patch("opta.layer.AwsEksProcessor")
+        mock_dict["aws-eks"] = mocked_eks_processor
+        mocked_dns_processor = mocker.patch("opta.layer.AwsDnsProcessor")
+        mock_dict["aws-dns"] = mocked_dns_processor
+        mocked_runx_processor = mocker.patch("opta.layer.RunxProcessor")
+        mock_dict["runx"] = mocked_runx_processor
+        mocked_aws_email_processor = mocker.patch("opta.layer.AwsEmailProcessor")
+        mock_dict["aws-ses"] = mocked_aws_email_processor
+        mocked_aws_documentdb_processor = mocker.patch(
+            "opta.layer.AwsDocumentDbProcessor"
+        )
+
+        mock_dict["aws-documentdb"] = mocked_aws_documentdb_processor
+
+        return mock_dict[module_type]
