705975db5a023e972a48ac819e40d55bee8cbefa
Sachin Agarwal <sachin@bigbitbus.com>
2022-01-03 15:40:48 -0500

    linting

--- opta/core/kubernetes.py
+++ opta/core/kubernetes.py
@@ -69,7 +69,7 @@ def _local_configure_kubectl(layer: "Layer") -> None:
         ["kubectl", "config", "use-context", "kind-opta-local-cluster"],
         check=True,
         capture_output=True,
-    ).stdout.decode('utf-8')
+    ).stdout.decode("utf-8")
 
 
 def _gcp_configure_kubectl(layer: "Layer") -> None:
