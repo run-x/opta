44cd77756b4f59b21aca252a49ccfafa3aa739b7
JD <jdpcbs@gmail.com>
2022-01-12 15:09:38 -0800

    lintfixing

--- tests/commands/test_kubectl.py
+++ tests/commands/test_kubectl.py
@@ -1,5 +1,3 @@
-from subprocess import CompletedProcess
-
 from click.testing import CliRunner
 from pytest_mock import MockFixture
 
@@ -33,43 +31,3 @@ def test_configure_kubectl(mocker: MockFixture) -> None:
     mocked_configure.assert_called_once_with(mocked_layer)
     mocked_load_opta_config_to_default.assert_called_once_with(mocked_layer)
     mocked_check_opta_file_exists.assert_called_once_with("opta.yaml")
-
-    # # Mock that the kubectl and aws comamnds exist in the env.
-    # mocked_ensure_installed = mocker.patch("opta.core.kubernetes.ensure_installed")
-    #
-    # # Mock aws commands, including fetching the current aws account id.
-    # fake_aws_account_id = 1234567890123
-    # mock_eks_client = mocker.Mock()
-    # mocker.patch("opta.core.kubernetes.boto3.client", return_value=mock_eks_client)
-    # mock_eks_client.describe_cluster.return_value = {
-    #     "cluster": {
-    #         "certificateAuthority": {"data": "ca-data"},
-    #         "endpoint": "eks-endpoint",
-    #     }
-    # }
-    # mocked_update_kubeconfig = "Updated context arn... in ../.kube/config"
-    # mocker.patch(
-    #     "opta.core.kubernetes.nice_run",
-    #     side_effect=[
-    #         CompletedProcess(
-    #             None, 0, mocked_update_kubeconfig.encode("utf-8")  # type: ignore
-    #         ),
-    #     ],
-    # )
-    #
-    # # Mock fetching the opta env aws account id.
-    # mocker.patch(
-    #     "opta.core.kubernetes._aws_get_cluster_env",
-    #     return_value=("us-east-1", [f"{fake_aws_account_id}"]),
-    # )
-    # # Mock fetching the cluster name
-    # mocker.patch(
-    #     "opta.core.kubernetes.get_terraform_outputs",
-    #     return_value={"parent.k8s_cluster_name": "main"},
-    # )
-    #
-    # runner = CliRunner()
-    # result = runner.invoke(configure_kubectl, [])
-    # mocked_layer_class.load_from_yaml.assert_called_with("opta.yaml", None)
-    # mocked_ensure_installed.assert_has_calls([mocker.call("kubectl")])
-    # assert result.exit_code == 0
