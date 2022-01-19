807ac5184dfcc185344e2b1e73c4daeb12735973
Sachin Agarwal <sachin@bigbitbus.com>
2022-01-19 06:34:42 -0500

    Terraform formate

--- modules/k8s_manifest/tf_module/main.tf
+++ modules/k8s_manifest/tf_module/main.tf
@@ -1,7 +1,6 @@
 provider "kubernetes" {
-  # config_path    = var.kubeconfig
-  # config_context = var.kubecontext
-https://471D9504BFE9596C4AD736A055C6213A.gr7.us-east-1.eks.amazonaws.com
+  config_path    = var.kubeconfig
+  config_context = var.kubecontext
 
 }
 
