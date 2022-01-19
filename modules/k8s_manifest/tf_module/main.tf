6ef4ab2d3e76ac9cdc3bbc81af76ac05dc205cb5
Sachin Agarwal <sachin@bigbitbus.com>
2022-01-19 06:00:42 -0500

    Created function for the config filename

--- modules/k8s_manifest/tf_module/main.tf
+++ modules/k8s_manifest/tf_module/main.tf
@@ -1,6 +1,8 @@
 provider "kubernetes" {
-  config_path    = var.kubeconfig
-  config_context = var.kubecontext
+  # config_path    = var.kubeconfig
+  # config_context = var.kubecontext
+https://471D9504BFE9596C4AD736A055C6213A.gr7.us-east-1.eks.amazonaws.com
+
 }
 
 resource "kubernetes_manifest" "manifest" {
