7f46265c1b3a27eb6f9042a8f8b5647125286c20
Juan Diego Palomino <jdpcbs@gmail.com>
2022-01-25 15:36:34 -0800

    Updating the azure and gcp nginx spec to have the client ip fix (#624)

--- modules/azure_k8s_base/tf_module/ingress_nginx.tf
+++ modules/azure_k8s_base/tf_module/ingress_nginx.tf
@@ -64,6 +64,7 @@ resource "helm_release" "ingress-nginx" {
         extraArgs : var.private_key == "" ? {} : { default-ssl-certificate : "ingress-nginx/secret-tls" }
         config : local.config
         podAnnotations : {
+          "config.linkerd.io/skip-inbound-ports" : "80,443" // NOTE: should be removed when this is fixed: https://github.com/linkerd/linkerd2/issues/4219
           "linkerd.io/inject" : "enabled"
         }
         resources : {
