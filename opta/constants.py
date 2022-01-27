3e579105b06a230b36b1a73f538d559a9737e083
Nilesh Sarupriya <sarupriyanilesh@outlook.com>
2022-01-27 21:57:12 +0530

    Refactoring special_formatter (#606)
    * Throw error when interpolation fails.
    
    * Lint Fixes
    
    * Use first element of Tuple
    
    * Throw error when interpolation fails.
    
    * Lint Fixes
    
    * Use first element of Tuple
    
    * Check changes only from opta apply
    
    * Added Global Variable
    
    * Refactor Opta Upgrade to Globals
    
    * Changes based on comments
    
    * change the debug log to info and just show the warning

--- opta/constants.py
+++ opta/constants.py
@@ -71,15 +71,3 @@ OPTA_INSTALL_URL = "https://docs.opta.dev/install.sh"
 successfull_upgrade = os.path.join(
     os.path.dirname(os.path.dirname(__file__)), ".successfull_upgrade"
 )
-
-
-class OptaUpgrade:
-    successful = False
-
-    @classmethod
-    def success(cls) -> None:
-        cls.successful = True
-
-    @classmethod
-    def unset(cls) -> None:
-        cls.successful = False
