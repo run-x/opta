2c6ed075fd2a4090461a9d88cb79f2f7f301835f
JD <jdpcbs@gmail.com>
2022-02-07 11:41:44 -0800

    Updated to 3.40

--- modules/aws_k8s_base/tf_module/ingress_nginx.tf
+++ modules/aws_k8s_base/tf_module/ingress_nginx.tf
@@ -3,7 +3,7 @@ resource "helm_release" "ingress-nginx" {
   chart            = "ingress-nginx"
   name             = "ingress-nginx"
   repository       = "https://kubernetes.github.io/ingress-nginx"
-  version          = "4.0.17"
+  version          = "3.40.0"
   namespace        = "ingress-nginx"
   create_namespace = true
   atomic           = true
