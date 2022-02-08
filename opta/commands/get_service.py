ac4e5225ac5fdf28254d8e3df2c188695aa8bf68
Nilesh Sarupriya <sarupriyanilesh@outlook.com>
2022-02-08 17:13:13 +0000

    add output file option

--- opta/commands/get_service.py
+++ opta/commands/get_service.py
@@ -1,10 +1,11 @@
+import os.path
+
 import click
 
 from opta.core.aws import AWS
 from opta.core.azure import Azure
 from opta.core.cloud_client import CloudClient
 from opta.core.gcp import GCP
-from opta.core.local import Local
 from opta.exceptions import UserErrors
 from opta.layer import Layer
 from opta.utils import check_opta_file_exists, logger
@@ -16,10 +17,27 @@ from opta.utils import check_opta_file_exists, logger
 @click.option("-o", "--output-file", default=None, help="Local file to download service configuration")
 def get_service(config: str, service_name: str, output_file: str) -> None:
     """
+    Get the Dependent Service configuration for a given Environment.
 
+    Examples:
+
+    - View all services:
+
+        opta get-service -c opta.yaml
+
+    - View a specific service:
+
+        opta get-service -c opta.yaml -n <service_name>
+
+    - Download a specific service:
+
+        opta get-service -c opta.yaml -n <service_name> -o <output_file>
     """
     config = check_opta_file_exists(config)
     layer = Layer.load_from_yaml(config, None)
+    abs_output_file = get_abs_path(output_file) if output_file is not None else None
+    overwrite_existing_file(abs_output_file)
+    layer.verify_cloud_credentials()
     cloud_client: CloudClient
     if layer.cloud == "aws":
         cloud_client = AWS(layer)
@@ -33,17 +51,36 @@ def get_service(config: str, service_name: str, output_file: str) -> None:
         raise Exception(f"Cannot handle upload config for cloud {layer.cloud}")
 
     child_service_names = cloud_client.list_child_config_names()
+
+    if not child_service_names:
+        logger.info("No child services found.")
+        return
+
     if service_name is None:
-        print("Following are the dependent service names")
-        for index, child_service_name in enumerate(child_service_names):
-            print(f"{index + 1}.\t{child_service_name}")
+        logger.info("Following are the dependent service names")
+        for child_service_name in child_service_names:
+            logger.info(f"  - {child_service_name}")
         return
 
     if service_name not in child_service_names:
         raise UserErrors("Service name invalid")
 
     requested_service_configuration = cloud_client.get_configuration_details(service_name)
-    if output_file is None:
-        print(requested_service_configuration)
+    if abs_output_file is None:
+        logger.info(requested_service_configuration)
         return
 
+    with open(abs_output_file, 'w') as f:
+        f.write(requested_service_configuration)
+
+
+def get_abs_path(file_path: str) -> str:
+    directory_path = os.path.dirname(file_path)
+    if not os.path.isdir(os.path.abspath(directory_path)):
+        raise UserErrors("Incorrect output file path")
+    return os.path.abspath(file_path)
+
+
+def overwrite_existing_file(file_path: str) -> None:
+    if os.path.isfile(file_path):
+        click.confirm("File with such name already exists. Do you want to overwrite it?", abort=True)
