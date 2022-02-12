d3411d8d0ced06b579003e28aab86413991ca6be
JD <jdpcbs@gmail.com>
2022-02-11 18:46:09 -0800

    Deleting EKS cloudwatch log group if pre-existing
    Cloudwatch log groups have this nasty habit of reviving themselves if there's ever one more log sent to them. This is why even though terraform deletes  them on opta destroy and we try to delete it again on post delete, they still stuck around. The consensus was to catch the log groups if they stuck around after a destroy and delete them there.

--- modules/aws_eks/tests/test_aws_eks.py
+++ modules/aws_eks/tests/test_aws_eks.py
@@ -1,6 +1,9 @@
 # type: ignore
 import os
 
+import boto3
+from botocore.config import Config
+from mypy_boto3_eks import EKSClient
 from pytest_mock import MockFixture
 
 from modules.aws_eks.aws_eks import AwsEksProcessor
@@ -104,6 +107,84 @@ class TestAwsEksModuleProcessor:
             AutoScalingGroupName="dummy_name", Granularity="1Minute"
         )
 
+    def test_delete_preexisting_cloudwatch_log_group_no_cluster_name(
+        self, mocker: MockFixture
+    ):
+        mocked_get_cluster_name = mocker.patch("modules.aws_eks.aws_eks.get_cluster_name")
+        mocked_get_cluster_name.return_value = None
+
+        layer = Layer.load_from_yaml(
+            os.path.join(
+                os.getcwd(), "tests", "fixtures", "dummy_data", "dummy_config_parent.yaml"
+            ),
+            None,
+        )
+        aws_eks_module = layer.get_module("awseks", 8)
+        AwsEksProcessor(aws_eks_module, layer).delete_preexisting_cloudwatch_log_group(
+            "us-east-1"
+        )
+        mocked_get_cluster_name.assert_called_once()
+
+    def test_delete_preexisting_cloudwatch_log_group_cluster_found(
+        self, mocker: MockFixture
+    ):
+        mocked_get_cluster_name = mocker.patch("modules.aws_eks.aws_eks.get_cluster_name")
+        mocked_get_cluster_name.return_value = "blah"
+        mocked_eks_client = mocker.Mock()
+        mocked_boto3 = mocker.patch("modules.aws_eks.aws_eks.boto3")
+        mocked_boto3.client.return_value = mocked_eks_client
+        mocked_eks_client.describe_cluster.return_value = {"cluster": {}}
+
+        layer = Layer.load_from_yaml(
+            os.path.join(
+                os.getcwd(), "tests", "fixtures", "dummy_data", "dummy_config_parent.yaml"
+            ),
+            None,
+        )
+        aws_eks_module = layer.get_module("awseks", 8)
+        AwsEksProcessor(aws_eks_module, layer).delete_preexisting_cloudwatch_log_group(
+            "us-east-1"
+        )
+        mocked_boto3.client.assert_called_once()
+        mocked_get_cluster_name.assert_called_once()
+
+    def test_delete_preexisting_cloudwatch_log_group_all_the_way(
+        self, mocker: MockFixture
+    ):
+        eks_client: EKSClient = boto3.client(
+            "eks", config=Config(region_name="us-east-1")
+        )
+        mocked_get_cluster_name = mocker.patch("modules.aws_eks.aws_eks.get_cluster_name")
+        mocked_get_cluster_name.return_value = "blah"
+        mocked_eks_client = mocker.Mock()
+        mocked_eks_client.exceptions = eks_client.exceptions
+        mocked_cloudwatch_client = mocker.Mock()
+        mocked_boto3 = mocker.patch("modules.aws_eks.aws_eks.boto3")
+        mocked_boto3.client.side_effect = [mocked_eks_client, mocked_cloudwatch_client]
+        mocked_eks_client.describe_cluster.side_effect = eks_client.exceptions.ResourceNotFoundException(
+            {}, "hi"
+        )
+        mocked_cloudwatch_client.describe_log_groups.return_value = {"logGroups": ["a"]}
+
+        layer = Layer.load_from_yaml(
+            os.path.join(
+                os.getcwd(), "tests", "fixtures", "dummy_data", "dummy_config_parent.yaml"
+            ),
+            None,
+        )
+        aws_eks_module = layer.get_module("awseks", 8)
+        AwsEksProcessor(aws_eks_module, layer).delete_preexisting_cloudwatch_log_group(
+            "us-east-1"
+        )
+        mocked_boto3.client.assert_has_calls(
+            [
+                mocker.call("eks", config=mocker.ANY),
+                mocker.call("logs", config=mocker.ANY),
+            ]
+        )
+        mocked_get_cluster_name.assert_called_once()
+        mocked_cloudwatch_client.delete_log_group.assert_called_once()
+
     def test_process(self, mocker: MockFixture):
         layer = Layer.load_from_yaml(
             os.path.join(
@@ -112,6 +193,9 @@ class TestAwsEksModuleProcessor:
             None,
         )
         aws_eks_module = layer.get_module("awseks", 8)
+        mocked_delete_preexisting_cloudwatch_log_group = mocker.patch(
+            "modules.aws_eks.aws_eks.AwsEksProcessor.delete_preexisting_cloudwatch_log_group"
+        )
         AwsEksProcessor(aws_eks_module, layer).process(8)
         assert (
             aws_eks_module.data["private_subnet_ids"]
@@ -121,3 +205,4 @@ class TestAwsEksModuleProcessor:
             aws_eks_module.data["kms_account_key_arn"]
             == "${{module.awsbase.kms_account_key_arn}}"
         )
+        mocked_delete_preexisting_cloudwatch_log_group.assert_called()
