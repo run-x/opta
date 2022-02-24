f43a2db94263e04d59c61a239e516ec04f3d3398
Juan Diego Palomino <jdpcbs@gmail.com>
2022-02-24 13:08:12 -0800

    Cronjob for local deployment and typo in gcp (#733)
    * Cronjob for local deployment and typo in gcp
    
    * linting

--- modules/local_k8s_service/tf_module/main.tf
+++ modules/local_k8s_service/tf_module/main.tf
@@ -52,6 +52,8 @@ resource "helm_release" "k8s-service" {
       keepPathPrefix : var.keep_path_prefix
       persistentStorage : var.persistent_storage
       ingressExtraAnnotations : var.ingress_extra_annotations
+      tolerations : var.tolerations
+      cron_jobs : var.cron_jobs
     })
   ]
   atomic          = true
