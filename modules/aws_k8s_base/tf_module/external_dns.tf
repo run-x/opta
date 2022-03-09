fad59f2d4c1d609c2273480a3a2e8ddbeb565f5d
Nilesh Sarupriya <sarupriyanilesh@outlook.com>
2022-03-09 17:19:31 +0000

    Use current latest version of External DNS helm chart

--- modules/aws_k8s_base/tf_module/external_dns.tf
+++ modules/aws_k8s_base/tf_module/external_dns.tf
@@ -53,7 +53,7 @@ resource "helm_release" "external-dns" {
   create_namespace = true
   atomic           = true
   cleanup_on_fail  = true
-  version          = "3.7.0"
+  version          = "6.1.8"
   values = [
     yamlencode({
       provider : "aws"
