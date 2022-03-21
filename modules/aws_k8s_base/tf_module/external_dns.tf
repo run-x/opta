2161e5277ac154a1455f5820d6c3831edb13238d
Nilesh Sarupriya <sarupriyanilesh@outlook.com>
2022-03-21 15:39:08 +0000

    Upgrade External-DNS version to latest

--- modules/aws_k8s_base/tf_module/external_dns.tf
+++ modules/aws_k8s_base/tf_module/external_dns.tf
@@ -53,7 +53,7 @@ resource "helm_release" "external-dns" {
   create_namespace = true
   atomic           = true
   cleanup_on_fail  = true
-  version          = "6.1.8"
+  version          = "6.2.1"
   values = [
     yamlencode({
       provider : "aws"
