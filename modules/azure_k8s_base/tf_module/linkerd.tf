9ace76d4c8e6d2221da741f94f833e81b1dc00fa
Nilesh Sarupriya <sarupriyanilesh@outlook.com>
2022-04-08 23:42:00 +0530

    Make Linkerd Certs Terraform sensitive (#805)
    * Make certificate information terraform sensitive
    
    * Azure Linkerd Cert Terraform sensitive
    
    Co-authored-by: Nilesh Sarupriya <nilesh@runx.dev>

--- modules/azure_k8s_base/tf_module/linkerd.tf
+++ modules/azure_k8s_base/tf_module/linkerd.tf
@@ -61,7 +61,7 @@ resource "helm_release" "linkerd" {
   repository = "https://helm.linkerd.io/stable"
   version    = "2.10.2"
 
-  set {
+  set_sensitive {
     name  = "identityTrustAnchorsPEM"
     value = tls_self_signed_cert.trustanchor_cert.cert_pem
   }
@@ -71,12 +71,12 @@ resource "helm_release" "linkerd" {
     value = tls_locally_signed_cert.issuer_cert.validity_end_time
   }
 
-  set {
+  set_sensitive {
     name  = "identity.issuer.tls.crtPEM"
     value = tls_locally_signed_cert.issuer_cert.cert_pem
   }
 
-  set {
+  set_sensitive {
     name  = "identity.issuer.tls.keyPEM"
     value = tls_private_key.issuer_key.private_key_pem
   }
