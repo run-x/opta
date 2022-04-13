965a5538612ce23349329bb0691ac04bb544a0e6
Sachin Agarwal <sachin@bigbitbus.com>
2022-04-13 09:12:15 -0400

    Removed linkerd from local, bugfixing local (#818)
    * Removed linkerd from local
    
    * Unset KUBECONFIG for local in circleci
    
    * unset KUBECONFIG
    
    * Added change to individual job instead

--- modules/local_base/tf_module/variables.tf
+++ modules/local_base/tf_module/variables.tf
@@ -13,16 +13,6 @@ variable "module_name" {
   type        = string
 }
 
-variable "linkerd_enabled" {
-  type    = bool
-  default = false
-}
-
-variable "linkerd_high_availability" {
-  type    = bool
-  default = false
-}
-
 variable "local_k8s_cluster_name" {
   type        = string
   default     = "kind-opta-local-cluster"
