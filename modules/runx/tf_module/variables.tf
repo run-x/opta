6089e348f4d356085a853c8f923685f99a568375
Juan Diego Palomino <jdpcbs@gmail.com>
2022-04-14 16:22:04 -0700

    Jd/runx module updates (#820)
    * Updates for the runx module
    
    * wip
    
    * wip
    
    * terraform fmt
    
    * Addressing cr

---
+++ modules/runx/tf_module/variables.tf
@@ -0,0 +1,22 @@
+variable "env_name" {
+  description = "Env name"
+  type        = string
+}
+
+variable "layer_name" {
+  description = "Layer name"
+  type        = string
+}
+
+variable "module_name" {
+  description = "Module name"
+  type        = string
+}
+
+variable "deploy_opta_agent" {
+  type = bool
+}
+
+variable "api_key" {
+  type = string
+}
