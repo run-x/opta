72bbd0c5a3bee1e927cc20ad33611ec0b3449982
Patrick Fiedler <patrick@runx.dev>
2022-04-15 13:21:46 -0500

    Fix tests failing if run with OPTA_DISABLE_REPORTING=1

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,36 @@
+import os
+import sys
+from typing import Any, Dict, Generator
 from pytest import fixture
 from pytest_mock import MockFixture
-
 
 # Most commands require terraform init. Mock it here.
 @fixture(autouse=True)
 def mock_terraform_init(mocker: MockFixture) -> None:
     mocker.patch("opta.core.terraform.Terraform.init")
+
+@fixture
+def hide_debug_mode() -> Generator:
+    """
+    Temporarily hides that we are running in a test by unsetting sys._called_from_test and
+    removing any env vars that might be used to disable reporting
+    """
+    # Grab original env vars
+    # Not using the imported constant to prevent errors when collecting tests
+    unset_environs = ["OPTA_DISABLE_REPORTING"]
+    original_environs: Dict[str, Any] = {}
+    for key in unset_environs:
+        original_environs[key] = os.environ.pop(key, None)
+
+    del sys._called_from_test
+
+    yield
+
+    sys._called_from_test = True
+
+    # Restore original values
+    for key, value in original_environs.items():
+        if value is None:
+            os.environ.pop(key, None)
+        else:
+            os.environ[key] = value
