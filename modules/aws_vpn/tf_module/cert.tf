2bb15aa883933c3ebf7e27f0b135554899bcb9a9
JD <jdpcbs@gmail.com>
2022-05-02 14:45:38 -0700

    minor clean up

--- modules/aws_vpn/tf_module/cert.tf
+++ modules/aws_vpn/tf_module/cert.tf
@@ -67,8 +67,8 @@ resource "tls_locally_signed_cert" "server_cert" {
 }
 
 resource "aws_acm_certificate" "server" {
-  private_key       = tls_private_key.server_issuer_key.private_key_pem
-  certificate_body  = tls_locally_signed_cert.server_issuer_cert.cert_pem
+  private_key       = tls_private_key.server_key.private_key_pem
+  certificate_body  = tls_locally_signed_cert.server_cert.cert_pem
   certificate_chain = tls_self_signed_cert.ca_cert.cert_pem
 }
 
