9cd7767b5fe4b19c10ee08a00b89139cf2de6601
Juan Diego Palomino <jdpcbs@gmail.com>
2022-05-02 14:40:58 -0700

    Update modules/aws_vpn/tf_module/cert.tf
    Co-authored-by: Patrick Fiedler <rabbitfang@users.noreply.github.com>

--- modules/aws_vpn/tf_module/cert.tf
+++ modules/aws_vpn/tf_module/cert.tf
@@ -49,8 +49,8 @@ resource "tls_cert_request" "server_req" {
   }
 }
 
-resource "tls_locally_signed_cert" "server_issuer_cert" {
-  cert_request_pem      = tls_cert_request.server_issuer_req.cert_request_pem
+resource "tls_locally_signed_cert" "server_cert" {
+  cert_request_pem      = tls_cert_request.server_req.cert_request_pem
   ca_key_algorithm      = tls_private_key.ca_key.algorithm
   ca_private_key_pem    = tls_private_key.ca_key.private_key_pem
   ca_cert_pem           = tls_self_signed_cert.ca_cert.cert_pem
