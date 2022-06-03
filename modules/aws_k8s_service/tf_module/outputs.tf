6709dc3355dd988d20cbfc89c72f65d23ef0813b
JD <jdpcbs@gmail.com>
2022-06-02 17:54:10 -0700

    K8s Service image name, tag and digest output
    K8s service will now output the image tag, name, and digest (if digest is used, tag is "", if tag is used digest is ""). Also removed some unused code

--- modules/aws_k8s_service/tf_module/outputs.tf
+++ modules/aws_k8s_service/tf_module/outputs.tf
@@ -9,5 +9,13 @@ output "docker_repo_url" {
 }
 
 output "current_image" {
-  value = local.image
+  value = split("@", split(":", local.image)[0])[0]
+}
+
+output "current_tag" {
+  value = length(regexall("[:]", local.image)) > 0 ? split(":", local.image)[1] : ""
+}
+
+output "current_digest" {
+  value = length(regexall("[@]", local.image)) > 0 ? split("@", local.image)[1] : ""
 }
