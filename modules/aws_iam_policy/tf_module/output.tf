8a9820c260a2400eb2842c24dbee71d67e2f1c8a
nsarupr <sarupriyanilesh@outlook.com>
2022-06-06 23:32:35 +0530

    Terraform fmt

--- modules/aws_iam_policy/tf_module/output.tf
+++ modules/aws_iam_policy/tf_module/output.tf
@@ -1,11 +1,11 @@
-output policy_arn {
+output "policy_arn" {
   value = aws_iam_policy.policy.arn
 }
 
-output policy_id {
+output "policy_id" {
   value = aws_iam_policy.policy.id
 }
 
-output policy_name {
+output "policy_name" {
   value = aws_iam_policy.policy.name
 }
--- modules/aws_iam_policy/tf_module/variables.tf
+++ modules/aws_iam_policy/tf_module/variables.tf
@@ -1,20 +1,20 @@
-variable env_name {
+variable "env_name" {
   description = "Env name"
   type        = string
 }
 
-variable layer_name {
+variable "layer_name" {
   description = "Layer name"
   type        = string
 }
 
-variable module_name {
+variable "module_name" {
   description = "Module name"
   type        = string
 }
 
-variable file {
+variable "file" {
   description = "Json file path containing the Policy"
-  type = string
-  default = ""
+  type        = string
+  default     = ""
 }
