2d37bb20afc27a5f627593f0be12b1ab2377113b
JD <jdpcbs@gmail.com>
2022-07-21 15:24:46 -0700

    updating gcp and azure too

--- modules/azure_k8s_base/tf_module/linkerd.tf
+++ modules/azure_k8s_base/tf_module/linkerd.tf
@@ -6,7 +6,6 @@ resource "tls_private_key" "trustanchor_key" {
 }
 
 resource "tls_self_signed_cert" "trustanchor_cert" {
-  key_algorithm         = tls_private_key.trustanchor_key.algorithm
   private_key_pem       = tls_private_key.trustanchor_key.private_key_pem
   validity_period_hours = 87600
   is_ca_certificate     = true
@@ -21,6 +20,9 @@ resource "tls_self_signed_cert" "trustanchor_cert" {
     "server_auth",
     "client_auth"
   ]
+  lifecycle {
+    ignore_changes = [subject]
+  }
 }
 
 resource "tls_private_key" "issuer_key" {
@@ -29,17 +31,18 @@ resource "tls_private_key" "issuer_key" {
 }
 
 resource "tls_cert_request" "issuer_req" {
-  key_algorithm   = tls_private_key.issuer_key.algorithm
   private_key_pem = tls_private_key.issuer_key.private_key_pem
 
   subject {
     common_name = "identity.linkerd.cluster.local"
   }
+  lifecycle {
+    ignore_changes = [subject]
+  }
 }
 
 resource "tls_locally_signed_cert" "issuer_cert" {
   cert_request_pem      = tls_cert_request.issuer_req.cert_request_pem
-  ca_key_algorithm      = tls_private_key.trustanchor_key.algorithm
   ca_private_key_pem    = tls_private_key.trustanchor_key.private_key_pem
   ca_cert_pem           = tls_self_signed_cert.trustanchor_cert.cert_pem
   validity_period_hours = 87600
