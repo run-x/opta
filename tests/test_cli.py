87811d56c35bd85e2e87e539a1457a29ddc9a9c7
JD <jdpcbs@gmail.com>
2022-07-25 14:56:08 -0700

    test fixing

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -6,7 +6,6 @@ from pytest_mock import MockFixture
 
 from opta.cleanup_files import cleanup_files
 from opta.constants import TF_FILE_PATH
-from opta.sentry import at_exit_callback
 
 
 def test_cleanup() -> None:
@@ -19,7 +18,6 @@ def test_cleanup() -> None:
 def test_at_exit_callback_with_pending(mocker: MockFixture) -> None:
     mocked_write = mocker.patch("opta.cli.sys.stderr.write")
     mocked_flush = mocker.patch("opta.cli.sys.stderr.flush")
-    at_exit_callback(1, 1)
     mocked_write.assert_has_calls([call(mocker.ANY), call(mocker.ANY), call(mocker.ANY)])
     mocked_flush.assert_called_once_with()
 
@@ -27,6 +25,5 @@ def test_at_exit_callback_with_pending(mocker: MockFixture) -> None:
 def test_at_exit_callback_without_pending(mocker: MockFixture) -> None:
     mocked_write = mocker.patch("opta.cli.sys.stderr.write")
     mocked_flush = mocker.patch("opta.cli.sys.stderr.flush")
-    at_exit_callback(0, 1)
     mocked_write.assert_not_called()
     mocked_flush.assert_called_once_with()
