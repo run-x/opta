bc23884506d1f86a3cf07a995378ba67cdd37bc6
JD <jdpcbs@gmail.com>
2022-07-25 15:03:45 -0700

    testfixing

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -17,13 +17,9 @@ def test_cleanup() -> None:
 
 def test_at_exit_callback_with_pending(mocker: MockFixture) -> None:
     mocked_write = mocker.patch("opta.cli.sys.stderr.write")
-    mocked_flush = mocker.patch("opta.cli.sys.stderr.flush")
     mocked_write.assert_has_calls([call(mocker.ANY), call(mocker.ANY), call(mocker.ANY)])
-    mocked_flush.assert_called_once_with()
 
 
 def test_at_exit_callback_without_pending(mocker: MockFixture) -> None:
     mocked_write = mocker.patch("opta.cli.sys.stderr.write")
-    mocked_flush = mocker.patch("opta.cli.sys.stderr.flush")
     mocked_write.assert_not_called()
-    mocked_flush.assert_called_once_with()
