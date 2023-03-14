a801a1fd6496e029d6ba253bf5f76681002ffb27
Jeev B <jeevb@users.noreply.github.com>
2023-03-13 19:56:38 -0700

    Enable atomic and cleanup_on_fail for nvidia_driver_installer helm chart

--- modules/gcp_k8s_base/tf_module/nvidia_driver_installer.tf
+++ modules/gcp_k8s_base/tf_module/nvidia_driver_installer.tf
@@ -1,7 +1,9 @@
 resource "helm_release" "nvidia_driver_installer" {
-  chart     = "${path.module}/nvidia-driver-installer"
-  name      = "nvidia-driver-installer"
-  namespace = "kube-system"
+  chart           = "${path.module}/nvidia-driver-installer"
+  name            = "nvidia-driver-installer"
+  namespace       = "kube-system"
+  atomic          = true
+  cleanup_on_fail = true
   values = [
     yamlencode({
       use_latest_version : var.nvidia_gpu_driver_version == "LATEST"
